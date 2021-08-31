a87c7f1c936cd05ffed009337de3bd9f437e8a60
Mathias Schäffler <mathias@fam-schaeffler.de>
2021-08-31 12:25:54 +0200

    interpolate

---
+++ my-math/mymath.js
@@ -0,0 +1,27 @@
+// Math functions for use in NodeRed function nodes
+
+exports.interpolate = function(input,curve)
+{
+    if( input > curve[0].input )
+    {
+        for( let i = 1; i<curve.length; i++ )
+        {
+            if( input <= curve[i].input )
+            {
+                const help1 = curve[i-1];
+                const help2 = curve[i];
+                return help1.output + (input-help1.input)*(help2.output-help1.output)/(help2.input-help1.input);
+            }
+        }
+        // größer als letzter Punkt
+        const help = curve[curve.length-1];
+        return input + help.output - help.input;
+    }
+    else
+    {
+        // kleiner als erster Punkt
+        const help = curve[0];
+        return input + help.output - help.input;
+    }
+}
+

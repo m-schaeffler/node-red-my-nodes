0db123fc430262446923e46ff238dede1661f105
Mathias Schäffler <mathias@fam-schaeffler.de>
2022-02-25 09:21:46 +0100

    dualLed 0.0.2

--- node-red-dualLed/ctrlDualLed.js
+++ node-red-dualLed/ctrlDualLed.js
@@ -2,10 +2,71 @@ module.exports = function(RED) {
 
     function CtrlDualLedNode(config) {
         RED.nodes.createNode(this,config);
-        this.config = config;
+        //this.config = config;
         var node = this;
+        var context = this.context();
+        const temp_warm = Number( config.temp_warm );
+        const temp_cold = Number( config.temp_cold );
+        const transition_short = Number( config.transition_short );
+        const transition_long  = Number( config.transition_long  );
 
         node.on('input', function(msg,send,done) {
+            let data = context.get( "data" ) ?? { turn:"off", temp:4000, brightness:100 };
+            let transition;
+            switch( typeof msg.payload )
+            {
+                case "string":
+                    switch( msg.payload )
+                    {
+                        case "on":
+                        case "off":
+                            data.turn = msg.payload;
+                            break;
+                        case "toggle":
+                            data.turn = data.turn=="on" ? "off" : "on";
+                            break;
+                    }
+                    transition = transition_short;
+                    break;
+                case "boolean":
+                    data.turn = msg.payload ? "on" : "off";
+                    transition = transition_short;
+                    break;
+                case "number":
+                    switch( msg.topic )
+                    {
+                        case "temp":
+                            data.temp = msg.payload;
+                            break;
+                        case "brightness":
+                            data.brightness = msg.payload;
+                            break;
+                    }
+                    transition = transition_long;
+                    break;
+                case "object":
+                    if( "temp" in msg.payload )
+                    {
+                        data.temp = msg.payload.temp;
+                    }
+                    if( "brightness" in msg.payload )
+                    {
+                        data.brightness = msg.payload.brightness;
+                    }
+                    transition = transition_long;
+                    break;
+            }
+            context.set( "data", data );
+
+            const x_warm = Math.round( data.brightness*(temp_cold-data.temp) / (temp_cold-temp_warm) );
+            const x_cold = data.brightness - x_warm;
+            const retain = msg.retain ?? false;
+
+            node.status({ fill: data.turn=="on"?"green":"gray", shape: "dot", text: x_warm+" / "+x_cold });
+            send( [
+                { topic:"warm LED", payload:{turn:data.turn,brightness:x_warm,transition:transition}, retain:retain },
+                { topic:"cold LED", payload:{turn:data.turn,brightness:x_cold,transition:transition}, retain:retain }
+            ] );
             done();
         });
     }
---
+++ node-red-dualLed/feedbackDualLed.js
@@ -0,0 +1,29 @@
+module.exports = function(RED) {
+
+    function FeedbackDualLedNode(config) {
+        RED.nodes.createNode(this,config);
+        //this.config = config;
+        var node = this;
+        const temp_warm = Number( config.temp_warm );
+        const temp_cold = Number( config.temp_cold );
+        const i_warm    = Number( config.indx_warm );
+        const i_cold    = Number( config.indx_cold );
+
+        node.on('input', function(msg,send,done) {
+            const x_warm = msg.payload.brightness[i_warm];
+            const x_cold = msg.payload.brightness[i_cold];
+
+            msg.payload.output     = msg.payload.output[i_warm];
+            msg.payload.brightness = x_warm + x_cold;
+            msg.payload.temp       = temp_warm + (temp_cold-temp_warm)/(x_warm+x_cold)*x_cold;
+            msg.payload.power      = msg.payload.power [i_warm] + msg.payload.power [i_cold];
+            msg.payload.energy     = msg.payload.energy[i_warm] + msg.payload.energy[i_cold];
+
+            node.status({ fill:msg.payload.output=="on"?"green":"gray", shape:"dot", text:`${msg.payload.brightness} / ${msg.payload.temp}` });
+            send( msg );
+            done();
+        });
+    }
+
+    RED.nodes.registerType("feedbackDualLed",FeedbackDualLedNode);
+}

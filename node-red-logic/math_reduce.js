8242835e15ea0402638e98bf1a41ea3978d820c2
Mathias Schäffler <mathias@fam-schaeffler.de>
2022-03-15 09:47:36 +0100

    math-reduce

--- node-red-logic/math_reduce.js
+++ node-red-logic/math_reduce.js
@@ -14,6 +14,71 @@ module.exports = function(RED) {
         node.on('input', function(msg,send,done) {
             const payload = Number( RED.util.getMessageProperty( msg, node.property ) );
 
+            if( msg.reset || msg.topic==="init" )
+            {
+                context.set( "data", {} );
+                node.status( "" );
+            }
+            else
+            {
+                if( ! isNaN( payload ) )
+                {
+                    let data = context.get( "data" ) ?? {};
+                    let item = data[msg.topic] ?? [];
+                    item.push( payload )
+                    data[msg.topic] = item;
+
+                    let count = 0;
+                    switch( node.algo )
+                    {
+                        case "prod": msg.payload = 1; break;
+                        case "min":  msg.payload = Number.MAX_SAFE_INTEGER; break;
+                        case "max":  msg.payload = Number.MIN_SAFE_INTEGER; break;
+                        default:     msg.payload = 0;
+                    }
+                    for( const key in data )
+                    {
+                        if( data[key].length >= node.minMean )
+                        {
+                            count++;
+                            while( data[key].length > node.maxMean )
+                            {
+                                data[key].shift();
+                            }
+                            const help = data[key].reduce( (accumulator,value)=>accumulator+value ) / data[key].length;
+                            switch( node.algo )
+                            {
+                                case "add":
+                                case "mean": msg.payload += help; break;
+                                case "prod": msg.payload *= help; break;
+                                case "min":  msg.payload  = Math.min( msg.payload, help ); break;
+                                case "max":  msg.payload  = Math.max( msg.payload, help ); break;
+                            }
+                        }
+                    }
+                    context.set( "data", data );
+
+                    if( count >= node.minData )
+                    {
+                        switch( node.algo )
+                        {
+                            case "mean": msg.payload /= count; break;
+                        }
+                        msg.count = count;
+                        node.status({fill:"green",shape:"dot",text:msg.payload});
+                        send( msg );
+                    }
+                    else
+                    {
+                        node.status({fill:"gray",shape:"dot",text:"to less data"});
+                    }
+                }
+                else
+                {
+                    node.status({fill:"red",shape:"dot",text:"payload is NaN"});
+                    node.error( "payload is NaN: "+msg.payload );
+                }
+            }
             done();
         });
     }

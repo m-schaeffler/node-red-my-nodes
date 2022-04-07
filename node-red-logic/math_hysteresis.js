bf785f2014bd312e6b3d6d84c8e8a5a9d529e6d9
Mathias Schäffler <mathias@fam-schaeffler.de>
2022-04-07 08:50:57 +0200

    hysteresis IDE

---
+++ node-red-logic/math_hysteresis.js
@@ -0,0 +1,53 @@
+module.exports = function(RED) {
+
+    function HysteresisEdgeNode(config) {
+        RED.nodes.createNode(this,config);
+        //this.config = config;
+        var node = this;
+        var context = this.context();
+        this.property  = config.property ?? "payload";
+        //this.threshold = config.threshold;
+        this.showState = config.showState;
+
+        node.on('input', function(msg,send,done) {
+            if( msg.reset || msg.topic==="init" )
+            {
+                context.set( "data", {} );
+                node.status( "" );
+            }
+            else
+            {
+                msg.payload = Number( RED.util.getMessageProperty( msg, node.property ) );
+                let status = { fill:"gray", shape:"dot", text:msg.payload };
+
+                if( ! isNaN( msg.payload ) )
+                {
+                /*
+                    let   data = context.get( "data" ) ?? {};
+                    const last = data[msg.topic] ?? Number.MIN_SAFE_INTEGER;
+                    if( msg.payload < this.threshold && this.threshold <= last )
+                    {
+                        status.fill = "green";
+                        send( msg );
+                    }
+                    data[msg.topic] = msg.payload;
+                    context.set( "data", data );
+                    */
+                }
+                else
+                {
+                    status.fill = "red";
+                    status.text = "not a Number";
+                }
+
+                if( node.showState )
+                {
+                    node.status( status );
+                }
+            }
+            done();
+        });
+    }
+
+    RED.nodes.registerType("hysteresisEdge",HysteresisEdgeNode);
+}

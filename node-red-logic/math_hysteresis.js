d1848a1709d5352317a3c4e7beadc5f891c20df0
Mathias Schäffler <mathias@fam-schaeffler.de>
2022-04-08 09:08:50 +0200

    hysteresis.js

--- node-red-logic/math_hysteresis.js
+++ node-red-logic/math_hysteresis.js
@@ -5,11 +5,14 @@ module.exports = function(RED) {
         //this.config = config;
         var node = this;
         var context = this.context();
-        this.property  = config.property ?? "payload";
-        //this.threshold = config.threshold;
-        this.showState = config.showState;
+        this.property        = config.property ?? "payload";
+        this.threshold_raise = config.threshold_raise;
+        this.threshold_fall  = config.threshold_fall;
+        this.initial         = config.initial;
+        this.showState       = config.showState;
 
         node.on('input', function(msg,send,done) {
+
             if( msg.reset || msg.topic==="init" )
             {
                 context.set( "data", {} );
@@ -22,17 +25,42 @@ module.exports = function(RED) {
 
                 if( ! isNaN( msg.payload ) )
                 {
-                /*
                     let   data = context.get( "data" ) ?? {};
-                    const last = data[msg.topic] ?? Number.MIN_SAFE_INTEGER;
-                    if( msg.payload < this.threshold && this.threshold <= last )
+                    const last = data[msg.topic];
+
+                    function sendMsg(edge)
                     {
                         status.fill = "green";
+                        data[msg.topic].edge = edge;
+                        msg.edge = edge;
                         send( msg );
                     }
-                    data[msg.topic] = msg.payload;
+
+                    if( last )
+                    {
+                        if( msg.payload > this.threshold_raise && this.threshold_raise >= last.value && last.edge != 'rising')
+                        {
+                            sendMsg( 'rising' );
+                        }
+                        else if( msg.payload < this.threshold_fall && this.threshold_fall <= last.value && last.edge != 'falling')
+                        {
+                            sendMsg( 'falling' );
+                        }
+                    }
+                    else
+                    {
+                        data[msg.topic] = {};
+                        if( ( this.initial == 'any' || this.initial == 'rising' ) && msg.payload > this.threshold_raise )
+                        {
+                            sendMsg( 'rising' );
+                        }
+                        else if( ( this.initial == 'any' || this.initial == 'falling' ) && msg.payload < this.threshold_fall )
+                        {
+                            sendMsg( 'falling' );
+                        }
+                    }
+                    data[msg.topic].value = msg.payload;
                     context.set( "data", data );
-                    */
                 }
                 else
                 {

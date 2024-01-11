56d502e50f206f23fe07d61f59d9f450496eba03
Mathias Schäffler <mschaeffler@gmx.de>
2024-01-11 13:02:53 +0100

    new test case mean

---
+++ node-red-logic/test/math_mean_spec.js
@@ -0,0 +1,194 @@
+var should = require("should");
+var helper = require("node-red-node-test-helper");
+var node   = require("../math_mean.js");
+
+describe( 'math_mean Node', function () {
+    "use strict";
+
+  beforeEach(function (done) {
+      helper.startServer(done);
+  });
+
+  afterEach(function(done) {
+      helper.unload().then(function() {
+          helper.stopServer(done);
+      });
+  });
+
+  it('should be loaded', function (done) {
+    var flow = [{ id: "n1", type: "mean", name: "test" }];
+    helper.load(node, flow, function () {
+      var n1 = helper.getNode("n1");
+      try {
+        n1.should.have.a.property('name', 'test');
+        n1.should.have.a.property('property', 'payload');
+        n1.should.have.a.property('propertyType', 'msg');
+        n1.should.have.a.property('deltaTime', 60);
+        n1.should.have.a.property('minData', 1);
+        n1.should.have.a.property('filter', 0);
+        n1.should.have.a.property('zeroIsZero', false);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should caclulate mean values', function (done) {
+    const numbers = [1000,10,99.9,100,100.1,1000,0];
+    var flow = [{ id: "n1", type: "mean", deltaTime:1, name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        try {
+          c++;
+          msg.should.have.property('payload',100.1);
+          msg.should.have.property('edge','rising');
+          if( c === 1 )
+          {
+            done();
+          }
+          else
+          {
+            done("too much messages");
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('deltaTime', 1);
+      }
+      catch(err) {
+        done(err);
+      }
+      for( const i of numbers )
+      {
+        n1.receive({ payload: i });
+      }
+    });
+  });
+/*
+  it('should not forward invalid data', function (done) {
+    var flow = [{ id: "n1", type: "raisingEdge", name: "test", threshold:100, wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        c++;
+        try {
+          msg.should.have.a.property('payload',5000);
+          if( c === 1 && msg.payload === 5000 )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.receive({ payload: 0 });
+      n1.receive({ invalid:true, payload: 1000 });
+      n1.receive({ invalid:true, payload: 0 });
+      n1.receive({ invalid:true, payload: 1000 });
+      n1.receive({ payload: undefined });
+      n1.receive({ payload: "FooBar" });
+      n1.receive({ payload: NaN });
+      n1.receive({ payload: 5000 });
+    });
+  });
+
+  it('should work with different topics', function (done) {
+    var flow = [{ id: "n1", type: "raisingEdge", threshold:100, name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        c++;
+        try {
+          msg.should.have.a.property('payload',c*1000);
+          msg.should.have.a.property('topic',c===1?"A":"B");
+          if( c === 2 && msg.payload === 2000 )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.receive({ topic:"A", payload: 0 });
+      n1.receive({ topic:"B", payload: 0 });
+      n1.receive({ topic:"A", payload: 1000 });
+      n1.receive({ topic:"B", payload: 2000 });
+    });
+  });
+
+  it('should have reset', function (done) {
+    var flow = [{ id: "n1", type: "raisingEdge", name: "test", threshold:100, wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        c++;
+        try {
+          msg.should.have.a.property('payload',5000);
+          if( c === 1 && msg.payload === 5000 )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.receive({ payload: 0 });
+      n1.receive({ reset: true });
+      n1.receive({ payload: 1000 });
+      n1.receive({ payload: 0 });
+      n1.receive({ topic: "init" });
+      n1.receive({ payload: 1000 });
+      n1.receive({ payload: 0 });
+      n1.receive({ payload: 5000 });
+    });
+  });
+
+  it('should have Jsonata', function (done) {
+    var flow = [{ id: "n1", type: "raisingEdge", name: "test", threshold:100, property:"payload+5", propertyType:"jsonata", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          msg.should.have.a.property('payload',98+5);
+          done();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('property', "payload+5");
+        n1.should.have.a.property('propertyType', "jsonata");
+      }
+      catch(err) {
+        done(err);
+      }
+      n1.receive({ payload: 0 });
+      n1.receive({ payload: 98 });
+    });
+  });
+*/
+});

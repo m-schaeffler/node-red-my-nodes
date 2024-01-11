1dd7e4d9b3f6664b7efe83cd1ca9e62933b0b14b
Mathias Schäffler <mschaeffler@gmx.de>
2024-01-11 08:36:50 +0100

    more unit tests for reduce

--- node-red-logic/test/math_reduce_spec.js
+++ node-red-logic/test/math_reduce_spec.js
@@ -66,9 +66,166 @@ describe( 'math_reduce Node', function () {
       }
     });
   });
-/*
+  
+  it('should mean values', function (done) {
+    const numbers = [1000,10,199.9,200,200.1,1000,100.1,100,99.9,0];
+    var flow = [{ id: "n1", type: "reduce", name: "test", algo:"mean", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      var s = 0;
+      n2.on("input", function (msg) {
+        try {
+          s += numbers[c++];
+          msg.should.have.property('payload',s/c);
+          msg.should.have.property('count',c)
+          if( c === numbers.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.should.have.a.property('algo', "mean");
+      for( const i in numbers )
+      {
+        n1.receive({ topic:i, payload: numbers[i] });
+      }
+    });
+  });
+  
+  it('should multiply values', function (done) {
+    const numbers = [1000,10,199.9,200,200.1,1000,100.1,100,99.9,0];
+    var flow = [{ id: "n1", type: "reduce", name: "test", algo:"prod", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      var s = 1;
+      n2.on("input", function (msg) {
+        try {
+          s *= numbers[c++];
+          msg.should.have.property('payload',s);
+          msg.should.have.property('count',c)
+          if( c === numbers.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.should.have.a.property('algo', "prod");
+      for( const i in numbers )
+      {
+        n1.receive({ topic:i, payload: numbers[i] });
+      }
+    });
+  });
+  
+  it('should min values', function (done) {
+    const numbers = [1000,10,199.9,200,200.1,1000,100.1,100,99.9,0];
+    var flow = [{ id: "n1", type: "reduce", name: "test", algo:"min", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      var s = 0;
+      n2.on("input", function (msg) {
+        try {
+          s = Math.min( s, numbers[c++] );
+          msg.should.have.property('payload',s);
+          msg.should.have.property('count',c)
+          if( c === numbers.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.should.have.a.property('algo', "min");
+      for( const i in numbers )
+      {
+        n1.receive({ topic:i, payload: numbers[i] });
+      }
+    });
+  });
+  
+  it('should max values', function (done) {
+    const numbers = [1000,10,199.9,200,200.1,1000,100.1,100,99.9,0];
+    var flow = [{ id: "n1", type: "reduce", name: "test", algo:"max", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      var s = 0;
+      n2.on("input", function (msg) {
+        try {
+          s = Math.max( s, numbers[c++] );
+          msg.should.have.property('payload',s);
+          msg.should.have.property('count',c)
+          if( c === numbers.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.should.have.a.property('algo', "max");
+      for( const i in numbers )
+      {
+        n1.receive({ topic:i, payload: numbers[i] });
+      }
+    });
+  });
+  
+  it('should check minData', function (done) {
+    const numbers = [1000,10,199.9,200,200.1,1000,100.1,100,99.9,0];
+    var flow = [{ id: "n1", type: "reduce", name: "test", algo:"add", minData:3, wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 34+17;
+      var s = 0;
+      n2.on("input", function (msg) {
+        try {
+          s += numbers[c++];
+          msg.should.have.property('payload',s);
+          msg.should.have.property('count',c)
+          if( c === numbers.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n1.should.have.a.property('minData', 3);
+      n1.receive({ topic:-2, payload: 34 })
+      n1.receive({ topic:-1, payload: 17 })
+      for( const i in numbers )
+      {
+        n1.receive({ topic:i, payload: numbers[i] });
+      }
+    });
+  });
+
   it('should not forward invalid data', function (done) {
-    var flow = [{ id: "n1", type: "hysteresisEdge", name: "test", threshold_raise:200, threshold_fall:100, wires: [["n2"]] },
+    var flow = [{ id: "n1", type: "reduce", name: "test", algo:"add", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
     helper.load(node, flow, function () {
       var n2 = helper.getNode("n2");
@@ -87,42 +244,13 @@ describe( 'math_reduce Node', function () {
           done(err);
         }
       });
-      n1.receive({ payload: 0 });
       n1.receive({ invalid:true, payload: 1000 });
       n1.receive({ invalid:true, payload: 0 });
       n1.receive({ invalid:true, payload: 1000 });
       n1.receive({ payload: undefined });
       n1.receive({ payload: "FooBar" });
       n1.receive({ payload: NaN });
-      n1.receive({ payload: 5000 });0
-    });
-  });
-
-  it('should work with different topics', function (done) {
-    var flow = [{ id: "n1", type: "hysteresisEdge", threshold_raise:200, threshold_fall:100, name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        c++;
-        try {
-          msg.should.have.a.property('payload',c*1000);
-          msg.should.have.a.property('topic',c===1?"A":"B");
-          if( c === 2 && msg.payload === 2000 )
-          {
-            done();
-          }
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      n1.receive({ topic:"A", payload: 0 });
-      n1.receive({ topic:"B", payload: 0 });
-      n1.receive({ topic:"A", payload: 1000 });
-      n1.receive({ topic:"B", payload: 2000 });
+      n1.receive({ payload: 5000 });
     });
   });
 
@@ -136,25 +264,40 @@ describe( 'math_reduce Node', function () {
       n2.on("input", function (msg) {
         c++;
         try {
-          msg.should.have.a.property('payload',5000);
-          if( c === 1 && msg.payload === 5000 )
+          msg.should.have.a.property('count',c===1?1:2);
+          switch( c )
           {
-            done();
+            case 1:
+              msg.should.have.a.property('payload',100);
+              break;
+            case 2:
+              msg.should.have.a.property('payload',101);
+              break;
+            case 3:
+              msg.should.have.a.property('payload',1);
+              break;
+            case 4:
+              msg.should.have.a.property('payload',5);
+              done();
+              break;
+            default:
+              done("too much messages");
           }
         }
         catch(err) {
           done(err);
         }
       });
-      n1.receive({ payload: 0 });
+      n1.receive({ topic:1, payload: 100 });
+      n1.receive({ topic:2, payload: 1 });
       n1.receive({ reset: true });
-      n1.receive({ payload: 1000 });
+      n1.receive({ topic:2, payload: 1 });
       n1.receive({ topic: "init" });
-      n1.receive({ payload: 150 });
-      n1.receive({ payload: 5000 });
+      n1.receive({ topic:3, payload: 5 });
     });
   });
 
+/*
   it('should have initial==rising', function (done) {
     var flow = [{ id: "n1", type: "hysteresisEdge", name: "test", threshold_raise:200, threshold_fall:100, initial:"rising", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
@@ -268,7 +411,7 @@ describe( 'math_reduce Node', function () {
   });
 
   it('should have Jsonata', function (done) {
-    var flow = [{ id: "n1", type: "hysteresisEdge", name: "test", threshold_raise:200, threshold_fall:100, property:"payload+5", propertyType:"jsonata", wires: [["n2"]] },
+    var flow = [{ id: "n1", type: "reduce", name: "test", threshold_raise:200, threshold_fall:100, property:"payload+5", propertyType:"jsonata", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
     helper.load(node, flow, function () {
       var n2 = helper.getNode("n2");

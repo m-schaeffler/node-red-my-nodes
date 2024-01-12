1c4055c1190569b7ae0c5042f030cbd589db6f27
Mathias Schäffler <mschaeffler@gmx.de>
2024-01-12 11:50:03 +0100

    test case for objects

--- node-red-logic/test/math_statistics_spec.js
+++ node-red-logic/test/math_statistics_spec.js
@@ -241,6 +241,35 @@ describe( 'math_statistics Node', function () {
     });
   });
 
+  it('should work with objects', function (done) {
+    var flow = [{ id: "n1", type: "statistics", name: "test", property:"payload.value", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      n2.on("input", function (msg) {
+        try {
+          msg.should.have.a.property('payload',98);
+          msg.stat.should.have.a.property('value',98+5);
+          msg.stat.should.have.a.property('count',1);
+          msg.stat.should.have.a.property('average',98+5);
+          done();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('property', "payload.value");
+        n1.should.have.a.property('propertyType', "msg");
+      }
+      catch(err) {
+        done(err);
+      }
+      n1.receive({ payload: {a:1,value:98,b:88} });
+    });
+  });
+
   it('should have Jsonata', function (done) {
     var flow = [{ id: "n1", type: "statistics", name: "test", property:"payload+5", propertyType:"jsonata", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];

ec51fced599defe7d02a5803feec25a5448d66bd
Mathias Schäffler <mathias@fam-schaeffler.de>
2024-01-15 10:48:17 +0100

    fix test case

--- node-red-contrib-lora/test/server_spec.js
+++ node-red-contrib-lora/test/server_spec.js
@@ -214,7 +214,7 @@ describe( 'lorawan-server Node', function () {
     });
   });
 
-  it('should receive PUSH_DATA message without data', function (done) {
+  it('should receive PUSH_DATA message with statistics', function (done) {
     var flow = [{ id: "n1", type: "lorawan-server", name: "test", wires: [["n2"],["n3"]] },
                 { id: "n2", type: "helper" },
                 { id: "n3", type: "helper" }];
@@ -342,7 +342,7 @@ describe( 'lorawan-server Node', function () {
             //console.log("send");
             //console.log(arg1);
             try {
-              arg1.should.be.eql(Buffer.from([2,19,132,1]));
+              arg1.should.be.eql(Buffer.from([2,213,236,1]));
               arg2.should.be.eql(30000);
               arg3.should.be.eql("10.11.12.13");
             }
@@ -364,8 +364,18 @@ describe( 'lorawan-server Node', function () {
       var n3 = helper.getNode("n3");
       n2.on("input", function (msg) {
         try {
-          console.log(msg.payload);
-          msg.should.fail();
+          //console.log(msg);
+          msg.should.have.property('topic','rx');
+          msg.should.have.property('gateway','a84041ffff1f8eb8');
+          msg.should.have.property('payload').which.is.an.Object();
+          msg.payload.should.have.property('time','2024-01-15T09:31:54.489512Z');
+          msg.payload.should.have.property('data','gNw7gwEAgEMK21gMW+DJWYGtM6N1ozM=');
+          msg.payload.should.have.property('tmst',228053275),
+          msg.payload.should.have.property('rfch',1),
+          msg.payload.should.have.property('modu','LORA');
+          msg.payload.should.have.property('datr','SF7BW125');
+          msg.payload.should.have.property('codr','4/5');
+          done();
         }
         catch(err) {
           done(err);
@@ -374,14 +384,7 @@ describe( 'lorawan-server Node', function () {
       n3.on("input", function (msg) {
         try {
           console.log(msg.payload);
-          msg.should.have.property('topic','stat');
-          msg.should.have.property('gateway','a84041ffff1f8eb8');
-          msg.should.have.property('payload').which.is.an.Object();
-          msg.payload.should.have.property('time','2024-01-15 08:18:05 UTC');
-          msg.payload.should.have.property('rxnb',0);
-          msg.payload.should.have.property('rxok',0),
-          msg.payload.should.have.property('rxfw',0),
-          msg.payload.should.have.property('desc','Dragino LoRaWAN Gateway');
+          msg.should.fail();
         }
         catch(err) {
           done(err);
@@ -392,7 +395,6 @@ describe( 'lorawan-server Node', function () {
         receiveLora.should.be.a.Function();
         receiveLora(Buffer.from([2,213,236,0,168,64,65,255,255,31,142,184,123,34,114,120,112,107,34,58,91,123,34,106,118,101,114,34,58,49,44,34,116,109,115,116,34,58,50,50,56,48,53,51,50,55,53,44,34,116,105,109,101,34,58,34,50,48,50,52,45,48,49,45,49,53,84,48,57,58,51,49,58,53,52,46,52,56,57,53,49,50,90,34,44,34,99,104,97,110,34,58,50,44,34,114,102,99,104,34,58,49,44,34,102,114,101,113,34,58,56,54,56,46,53,48,48,48,48,48,44,34,109,105,100,34,58,48,44,34,115,116,97,116,34,58,49,44,34,109,111,100,117,34,58,34,76,79,82,65,34,44,34,100,97,116,114,34,58,34,83,70,55,66,87,49,50,53,34,44,34,99,111,100,114,34,58,34,52,47,53,34,44,34,114,115,115,105,115,34,58,45,53,49,44,34,108,115,110,114,34,58,55,46,50,44,34,102,111,102,102,34,58,48,44,34,114,115,115,105,34,58,45,53,49,44,34,115,105,122,101,34,58,50,51,44,34,100,97,116,97,34,58,34,103,78,119,55,103,119,69,65,103,69,77,75,50,49,103,77,87,43,68,74,87,89,71,116,77,54,78,49,111,122,77,61,34,125,93,125]),{address:"10.11.12.13",port:30000});
         n1.should.have.a.property('gateway',null);
-        done();
         dgram.createSocket.restore();
       }
       catch(err) {

cb80b1e50925ac78dcf65c28df9430cbaa12d857
Mathias Schäffler <mschaeffler@gmx.de>
2024-01-15 13:52:21 +0100

    two more test cases

--- node-red-contrib-lora/test/encoder_spec.js
+++ node-red-contrib-lora/test/encoder_spec.js
@@ -383,4 +383,133 @@ describe( 'lorawan-packet-encoder Node', function () {
     });
   });
 
+
+  it('should set the default framecounter', function (done) {
+    var flow = [{ id:'flow', type:'tab' },
+                { id: "n1", type: "lorawan-packet-encoder", keys:"nk", name: "test", wires: [["n2"],["n3"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" },
+                { id: "n3", type: "helper", z: "flow" },
+                { id: "nk", type: "lorawan-keys", keys:keys.keys, name: "TestKeys", z: "flow" }];
+    helper.load([node,nodeKey], flow, function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var n3 = helper.getNode("n3");
+      var nk = helper.getNode("nk");
+      var c = 0;
+      n2.on("input", function (msg) {
+        try {
+          c++;
+          console.log(msg.payload);
+          msg.should.have.a.property('topic',c===1?"Foo 1":"Bar 1");
+          msg.should.have.a.property('payload').which.is.an.Object();
+          msg.payload.should.have.a.property('txpk').which.is.an.Object();
+          msg.payload.txpk.should.have.a.property('freq',869.525);
+          msg.payload.txpk.should.have.a.property('powe',14);
+          msg.payload.txpk.should.have.a.property('modu','LORA');
+          msg.payload.txpk.should.have.a.property('datr','SF7BW125');
+          msg.payload.txpk.should.have.a.property('codr','4/5');
+          msg.payload.txpk.should.have.a.property('ipol',true);
+          msg.payload.txpk.should.have.a.property('size',17);
+          msg.payload.txpk.should.have.a.property('data','YHhWNBIAAQAGDLyYVLOxCmg=');
+          msg.payload.txpk.should.have.a.property('imme',true);
+          msg.payload.txpk.should.not.have.a.property('rfch');
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n3.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.have.a.property('topic',"framecounter");
+          msg.should.have.a.property('payload').which.is.an.Object();
+          msg.payload.should.have.a.property('12345678',151);
+          if( c===1 ) {
+            msg.payload.should.not.have.a.property('0000abcd');
+          } else {
+            msg.payload.should.have.a.property('0000abcd',151);
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('keyconf').which.is.an.Object();
+        should.not.exist( n1.context().get("counters", "storeInFile") );
+        n1.receive({ framecounter: 150 });
+        n1.receive({ payload: { device_address:"12345678", data:[1,2,3,4], port:6 } });
+        n1.receive({ payload: { device_address:"0000abcd", data:[1,2,3,4], port:6 } });
+        should.exist( n1.context().get("counters", "storeInFile") );
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should set the framecounter', function (done) {
+    var flow = [{ id:'flow', type:'tab' },
+                { id: "n1", type: "lorawan-packet-encoder", keys:"nk", name: "test", wires: [["n2"],["n3"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" },
+                { id: "n3", type: "helper", z: "flow" },
+                { id: "nk", type: "lorawan-keys", keys:keys.keys, name: "TestKeys", z: "flow" }];
+    helper.load([node,nodeKey], flow, function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var n3 = helper.getNode("n3");
+      var nk = helper.getNode("nk");
+      var c = 0;
+      n2.on("input", function (msg) {
+        try {
+          c++;
+          console.log(msg.payload);
+          msg.should.have.a.property('topic',c===1?"Foo 1":"Bar 1");
+          msg.should.have.a.property('payload').which.is.an.Object();
+          msg.payload.should.have.a.property('txpk').which.is.an.Object();
+          msg.payload.txpk.should.have.a.property('freq',869.525);
+          msg.payload.txpk.should.have.a.property('powe',14);
+          msg.payload.txpk.should.have.a.property('modu','LORA');
+          msg.payload.txpk.should.have.a.property('datr','SF7BW125');
+          msg.payload.txpk.should.have.a.property('codr','4/5');
+          msg.payload.txpk.should.have.a.property('ipol',true);
+          msg.payload.txpk.should.have.a.property('size',17);
+          msg.payload.txpk.should.have.a.property('data','YHhWNBIAAQAGDLyYVLOxCmg=');
+          msg.payload.txpk.should.have.a.property('imme',true);
+          msg.payload.txpk.should.not.have.a.property('rfch');
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n3.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.have.a.property('topic',"framecounter");
+          msg.should.have.a.property('payload').which.is.an.Object();
+          msg.payload.should.have.a.property('12345678',151);
+          if( c===1 ) {
+            msg.payload.should.not.have.a.property('0000abcd');
+          } else {
+            msg.payload.should.have.a.property('0000abcd',151);
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('keyconf').which.is.an.Object();
+        should.not.exist( n1.context().get("counters", "storeInFile") );
+        n1.receive({ framecounter: {"12345678":150,"0000abcd":125} });
+        n1.receive({ payload: { device_address:"12345678", data:[1,2,3,4], port:6 } });
+        n1.receive({ payload: { device_address:"0000abcd", data:[1,2,3,4], port:6 } });
+        should.exist( n1.context().get("counters", "storeInFile") );
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
 });

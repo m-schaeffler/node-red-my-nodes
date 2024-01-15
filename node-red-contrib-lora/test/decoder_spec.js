a7f7b78cb02012e107f803bd0eec35521e779eb4
Mathias Schäffler <mathias@fam-schaeffler.de>
2024-01-15 18:42:09 +0100

    two more test cases

--- node-red-contrib-lora/test/decoder_spec.js
+++ node-red-contrib-lora/test/decoder_spec.js
@@ -51,7 +51,7 @@ describe( 'lorawan-packet-decoder Node', function () {
     });
   });
 
-  it('should store messages', function (done) {
+  it('should decode unconfirmed messages', function (done) {
     var flow = [{ id:'flow', type:'tab' },
                 { id: "n1", type: "lorawan-packet-decoder", keys:"nk", name: "test", wires: [["n2"],["n3"],["n4"],["n5"]], z: "flow" },
                 { id: "n2", type: "helper", z: "flow" },
@@ -66,10 +66,28 @@ describe( 'lorawan-packet-decoder Node', function () {
       var n4 = helper.getNode("n4");
       var n5 = helper.getNode("n5");
       var nk = helper.getNode("nk");
+      var c = 0;
       n2.on("input", function (msg) {
         try {
-          console.log(msg.payload);
-          msg.should.fail();
+          c++;
+          //console.log(msg);
+          msg.should.have.a.property('topic',c<3?"Foo 1":"Bar 1");
+          msg.should.have.a.property('payload').which.is.an.Object();
+          msg.payload.should.have.a.property('device_address',c<3?'12345678':'0000abcd');
+          msg.payload.should.have.a.property('frame_count',c<3?c:1);
+          msg.payload.should.have.a.property('port',c<3?6:1);
+          msg.payload.should.have.a.property('mtype','Unconfirmed Data Down');
+          msg.payload.should.have.a.property('confirmed',false);
+          msg.payload.should.have.a.property('type',c<3?'foo':'bar');
+          msg.payload.should.have.a.property('name',msg.topic);
+          msg.payload.should.have.a.property('data',c<3?[1,2,3,4]:[255]);
+          msg.payload.should.have.a.property('rxpk').which.is.an.Object();
+          msg.payload.rxpk.should.have.a.property('data').which.is.String();
+          msg.payload.rxpk.should.have.a.property('test','UnitTest');
+          msg.payload.rxpk.should.have.a.property('time');
+          if( c === 3 ) {
+            done();
+          }
         }
         catch(err) {
           done(err);
@@ -105,8 +123,148 @@ describe( 'lorawan-packet-decoder Node', function () {
       try {
         n1.should.have.a.property('keyconf').which.is.an.Object();
         should.not.exist( n1.context().flow.get("sendqueue") );
+        n1.receive({ payload: { data:"YHhWNBIAAQAGDLyYVLOxCmg=", test:"UnitTest" } });
+        n1.receive({ payload: { data:"YHhWNBIAAgAGcI+ruBkP3Oc=", test:"UnitTest" } });
+        n1.receive({ payload: { data:"YM2rAAAAAQABDpRwwJ0=", test:"UnitTest" } });
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should not decode invalid messages', function (done) {
+    var flow = [{ id:'flow', type:'tab' },
+                { id: "n1", type: "lorawan-packet-decoder", keys:"nk", name: "test", wires: [["n2"],["n3"],["n4"],["n5"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" },
+                { id: "n3", type: "helper", z: "flow" },
+                { id: "n4", type: "helper", z: "flow" },
+                { id: "n5", type: "helper", z: "flow" },
+                { id: "nk", type: "lorawan-keys", keys:keys.keys, name: "TestKeys", z: "flow" }];
+    helper.load([node,nodeKey], flow, function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var n3 = helper.getNode("n3");
+      var n4 = helper.getNode("n4");
+      var n5 = helper.getNode("n5");
+      var nk = helper.getNode("nk");
+      n2.on("input", function (msg) {
+        try {
+          console.log(msg);
+          msg.should.fail();1
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n3.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.fail();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n4.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.fail();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n5.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.fail();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('keyconf').which.is.an.Object();
+        should.not.exist( n1.context().flow.get("sendqueue") );
+        n1.receive({});
         n1.receive({ payload: {} });
+        n1.receive({ payload: { data:"1234" } });
+        //n1.receive({ payload: { data:"YHlWNBIAAQAGDLyYVLOxCmg=" } });
+        n1.receive({ payload: { data:"1234567890" } });
         done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should forward unknown devices', function (done) {
+    var flow = [{ id:'flow', type:'tab' },
+                { id: "n1", type: "lorawan-packet-decoder", keys:"nk", name: "test", wires: [["n2"],["n3"],["n4"],["n5"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" },
+                { id: "n3", type: "helper", z: "flow" },
+                { id: "n4", type: "helper", z: "flow" },
+                { id: "n5", type: "helper", z: "flow" },
+                { id: "nk", type: "lorawan-keys", keys:keys.keys, name: "TestKeys", z: "flow" }];
+    helper.load([node,nodeKey], flow, function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var n3 = helper.getNode("n3");
+      var n4 = helper.getNode("n4");
+      var n5 = helper.getNode("n5");
+      var nk = helper.getNode("nk");
+      n2.on("input", function (msg) {
+        try {
+          console.log(msg);
+          msg.should.fail();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n3.on("input", function (msg) {
+        try {
+          //console.log(msg);
+          msg.should.have.a.property('topic',"External");
+          msg.should.have.a.property('payload').which.is.an.Object();
+          msg.payload.should.have.a.property('device_address','12345679');
+          msg.payload.should.have.a.property('frame_count',1);
+          msg.payload.should.have.a.property('port',6);
+          msg.payload.should.have.a.property('mtype','Unconfirmed Data Down');
+          msg.payload.should.have.a.property('confirmed',false);
+          msg.payload.should.have.a.property('rxpk').which.is.an.Object();
+          msg.payload.rxpk.should.have.a.property('data','YHlWNBIAAQAGDLyYVLOxCmg=');
+          msg.payload.rxpk.should.have.a.property('time');
+          done();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n4.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.fail();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      n5.on("input", function (msg) {
+        try {
+          console.log(msg.payload);
+          msg.should.fail();
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('keyconf').which.is.an.Object();
+        should.not.exist( n1.context().flow.get("sendqueue") );
+        n1.receive({ topic:"External", payload: { data:"YHlWNBIAAQAGDLyYVLOxCmg=" } });
       }
       catch(err) {
         done(err);

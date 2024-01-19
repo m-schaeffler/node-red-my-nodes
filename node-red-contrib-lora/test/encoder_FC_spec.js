efc2a3d36fb1dae7cd7ae22ccca453ef9c824225
Mathias Schäffler <mathias@fam-schaeffler.de>
2024-01-19 12:15:57 +0100

    improve test case

--- node-red-contrib-lora/test/encoder_FC_spec.js
+++ node-red-contrib-lora/test/encoder_FC_spec.js
@@ -48,9 +48,10 @@ describe( 'lorawan-packet-encoder Node', function () {
       var n3 = helper.getNode("n3");
       var nk = helper.getNode("nk");
       var c = 0;
+      var spy = sinon.spy(lora_packet,'fromFields');
       n2.on("input", function (msg) {
         try {
-          const data =['YHhWNBIAAQAGDLyYVLOxCmg=','YHhWNBIAAgAGcI+ruBkP3Oc='];
+          const data =['YHhWNBIAEAAGaqNYXlaqgw8=','YHhWNBIAIAAGdjJd4llLFsc='];
           c++;
           //console.log(msg);
           msg.should.have.a.property('topic',"Foo 1");
@@ -59,7 +60,7 @@ describe( 'lorawan-packet-encoder Node', function () {
           msg.payload.txpk.should.have.a.property('freq',869.525);
           msg.payload.txpk.should.have.a.property('powe',14);
           msg.payload.txpk.should.have.a.property('modu','LORA');
-          msg.payload.txpk.should.have.a.property('datr',SF7BW125');
+          msg.payload.txpk.should.have.a.property('datr','SF7BW125');
           msg.payload.txpk.should.have.a.property('codr','4/5');
           msg.payload.txpk.should.have.a.property('ipol',true);
           msg.payload.txpk.should.have.a.property('size',17);
@@ -73,16 +74,20 @@ describe( 'lorawan-packet-encoder Node', function () {
       });
       n3.on("input", function (msg) {
         try {
-          console.log(msg);
+          //console.log(msg);
           msg.should.have.a.property('topic',"framecounter");
           msg.should.have.a.property('payload').which.is.an.Object();
           switch(c)
           {
             case 1:
               msg.payload.should.have.a.property('12345678',0x10010);
+              spy.args[0][0].FCnt.should.be.eql( 16 );
+              spy.args[0][4].should.be.eql(Buffer.from([1,0]));
               break;
             case 2:
               msg.payload.should.have.a.property('12345678',0x2000020);
+              spy.args[1][0].FCnt.should.be.eql( 32 );
+              spy.args[1][4].should.be.eql(Buffer.from([0,2]));
               done()
               break;
           }
@@ -99,12 +104,18 @@ describe( 'lorawan-packet-encoder Node', function () {
         n1.receive({ framecounter: fc });
         should.exist( n1.context().get("counters", "storeInFile") );
         n1.receive({ payload: { device_address:"12345678", data:[1,2,3,4], port:6 } });
+        //console.log(spy.args);
+        //
         fc["12345678"] = 0x200001F;
         n1.receive({ framecounter: fc });
         n1.receive({ payload: { device_address:"12345678", data:[1,2,3,4], port:6 } });
-        should.exist( n1.context().get("counters", "storeInFile") );
+        //console.log(spy.args);
+        spy.args[0][4].should.be.eql(Buffer.from(1,0));
+        spy.args[1][4].should.be.eql(Buffer.from(1,0));
+        lora-packet.fromFields.restore();
       }
       catch(err) {
+        lora-packet.fromFields.restore();
         done(err);
       }
     });

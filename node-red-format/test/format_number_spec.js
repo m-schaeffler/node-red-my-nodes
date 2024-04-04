ea12c67ce2ae340caf7677d46706c94753833e43
Mathias Schäffler <mschaeffler@gmx.de>
2024-04-04 14:09:04 +0200

    formNumbers: more test cases

--- node-red-format/test/format_number_spec.js
+++ node-red-format/test/format_number_spec.js
@@ -99,6 +99,41 @@ describe( 'format_number Node', function () {
     });
   });
 
+  it('should forward numbers rounded to two digits and changed decimal', function (done) {
+    const numbers = [-1,0,1,12.345,-12.345,"-1","0","1","34.5","-34.5",true,false,null];
+    var flow = [{ id: "n1", type: "formatNumber", decimal: ",", digits: 2, name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg.payload);
+        try {
+          msg.should.have.property('payload',Number(numbers[c]).toFixed(2).replace('.',','));
+          if( ++c === numbers.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('decimal', ",");
+        n1.should.have.a.property('digits', 2);
+      }
+      catch(err) {
+        done(err);
+      }
+      for( const i of numbers )
+      {
+        n1.receive({ payload: i });
+      }
+    });
+  });
+
   it('should forward numbers with an added unit', function (done) {
     const numbers = [-1,0,1,12.345,-12.345,"-1","0","1","34.5","-34.5",true,false,null];
     var flow = [{ id: "n1", type: "formatNumber", unit: "VAr", name: "test", wires: [["n2"]] },
@@ -122,6 +157,76 @@ describe( 'format_number Node', function () {
       });
       try {
         n1.should.have.a.property('unit', '\u2009VAr');
+      }
+      catch(err) {
+        done(err);
+      }
+      for( const i of numbers )
+      {
+        n1.receive({ payload: i });
+      }
+    });
+  });
+
+  it('should forward numbers without grouping', function (done) {
+    const numbers = [-12345,123,1234,12345,123456,1234567,12345678,1234.1,1233.9,0,1,-1];
+    const results = ["-12345","123","1234","12345","123456","1234567","12345678","1234","1234","0","1","-1"];
+    var flow = [{ id: "n1", type: "formatNumber", grouping: "", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg.payload);
+        try {
+          msg.should.have.property('payload',results[c]);
+          if( ++c === results.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('grouping', "");
+      }
+      catch(err) {
+        done(err);
+      }
+      for( const i of numbers )
+      {
+        n1.receive({ payload: i });
+      }
+    });
+  });
+
+  it('should forward numbers with grouping', function (done) {
+    const numbers = [-12345,123,1234,12345,123456,1234567,12345678,1234.1,1233.9,0,1,-1];
+    const results = ["-12'345","123","1'234","12'345","123'456","1'234'567","12'345'678","1'234","1'234","0","1","-1"];
+    var flow = [{ id: "n1", type: "formatNumber", grouping: "'", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg.payload);
+        try {
+          msg.should.have.property('payload',results[c]);
+          if( ++c === results.length )
+          {
+            done();
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('grouping', "$1'");
       }
       catch(err) {
         done(err);

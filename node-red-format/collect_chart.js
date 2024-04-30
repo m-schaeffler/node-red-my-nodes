c895e21043275d471cfd98d85d6a6b1d44ddbfb2
Mathias Schäffler <mathias@fam-schaeffler.de>
2024-04-30 18:49:20 +0200

    collectChart: first version

---
+++ node-red-format/collect_chart.js
@@ -0,0 +1,69 @@
+module.exports = function(RED) {
+
+    function CollectChartNode(config) {
+        RED.nodes.createNode(this,config);
+        //this.config = config;
+        var node = this;
+        this.property     = config.property ?? "payload";
+        this.propertyType = config.propertyType ?? "msg";
+        this.unit         = config.topics ?? "[]";
+        this.hours        = Number( config.digits ?? 24 );
+        this.showState    = Boolean( config.showState );
+        if( this.propertyType === "jsonata" )
+        {
+            try {
+                this.propertyPrepared = RED.util.prepareJSONataExpression( this.property, this );
+            }
+            catch (e) {
+                node.error(RED._("debug.invalid-exp", {error: this.property}));
+                return;
+            }
+        }
+        node.status( "" );
+
+        node.on('input', function(msg,send,done) {
+            if( msg.invalid )
+            {
+                done();
+                return;
+            }
+            function getPayload(callback)
+            {
+                if( node.propertyPrepared )
+                {
+                    RED.util.evaluateJSONataExpression( node.propertyPrepared, msg, function(err, value)
+                    {
+                        if( err )
+                        {
+                            done( RED._("debug.invalid-exp", {error: editExpression}) );
+                        }
+                        else
+                        {
+                            callback( value );
+                        }
+                    } );
+                }
+                else
+                {
+                    callback( RED.util.getMessageProperty( msg, node.property ) );
+                }
+            }
+            getPayload( function(value)
+            {
+                const number = Number( value );
+                let   status = {};
+                if( ! isNaN( number ) )
+                {
+                }
+                if( node.showState )
+                {
+                    status.text = msg.payload;
+                    node.status( status );
+                }
+                done();
+            } );
+        });
+    }
+
+    RED.nodes.registerType("collectChart",CollectChartNode);
+}

c83f77608e04adb93ba6d9b292a7e6498020898c
Mathias Schäffler <mathias@fam-schaeffler.de>
2024-05-02 11:19:25 +0200

    collectChart: UT for invalid data

--- node-red-format/test/collect_chart_spec.js
+++ node-red-format/test/collect_chart_spec.js
@@ -136,168 +136,34 @@ describe( 'collect_chart Node', function () {
       done();
     });
   });
-/*
-  it('should forward numbers rounded to two digits and changed decimal', function (done) {
-    const numbers = [-1,0,1,12.345,-12.345,"-1","0","1","34.5","-34.5",true,false,null];
-    var flow = [{ id: "n1", type: "formatNumber", decimal: ",", digits: 2, name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',Number(numbers[c]).toFixed(2).replace('.',','));
-          if( ++c === numbers.length )
-          {
-            done();
-          }
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('decimal', ",");
-        n1.should.have.a.property('digits', 2);
-      }
-      catch(err) {
-        done(err);
-      }
-      for( const i of numbers )
-      {
-        n1.receive({ payload: i });
-      }
-    });
-  });
 
-  it('should forward numbers with an added unit', function (done) {
-    const numbers = [-1,0,1,12.345,-12.345,"-1","0","1","34.5","-34.5",true,false,null];
-    var flow = [{ id: "n1", type: "formatNumber", unit: "VAr", name: "test", wires: [["n2"]] },
+  it('should not collect invalid data', function (done) {
+    this.timeout( 10000 );
+    var flow = [{ id: "n1", type: "collectChart", cyclic: 1, name: "test", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
-    helper.load(node, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',Number(numbers[c]).toFixed(0)+'\u2009VAr');
-          if( ++c === numbers.length )
-          {
-            done();
-          }
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('unit', '\u2009VAr');
-      }
-      catch(err) {
-        done(err);
-      }
-      for( const i of numbers )
-      {
-        n1.receive({ payload: i });
-      }
-    });
-  });
-
-  it('should forward numbers without grouping', function (done) {
-    const numbers = [-12345,123,1234,12345,123456,1234567,12345678,1234.1,1233.9,0,1,-1];
-    const results = ["-12345","123","1234","12345","123456","1234567","12345678","1234","1234","0","1","-1"];
-    var flow = [{ id: "n1", type: "formatNumber", grouping: "", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',results[c]);
-          if( ++c === results.length )
-          {
-            done();
-          }
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('grouping', "");
-      }
-      catch(err) {
-        done(err);
-      }
-      for( const i of numbers )
-      {
-        n1.receive({ payload: i });
-      }
-    });
-  });
-
-  it('should forward numbers with grouping', function (done) {
-    const numbers = [-12345,123,1234,12345,123456,1234567,12345678,1234.1,1233.9,0,1,-1];
-    const results = ["-12'345","123","1'234","12'345","123'456","1'234'567","12'345'678","1'234","1'234","0","1","-1"];
-    var flow = [{ id: "n1", type: "formatNumber", grouping: "'", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',results[c]);
-          if( ++c === results.length )
-          {
-            done();
-          }
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('grouping', "$1'");
-      }
-      catch(err) {
-        done(err);
-      }
-      for( const i of numbers )
-      {
-        n1.receive({ payload: i });
-      }
-    });
-  });
-
-  it('should not forward invalid data', function (done) {
-    var flow = [{ id: "n1", type: "formatNumber", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, function () {
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
       var c = 0;
       n2.on("input", function (msg) {
         try {
-          msg.should.have.a.property('payload',"255");
-          done();
+          c++;
+          c.should.match(1);
+          msg.should.have.property('init',true);
+          msg.should.have.a.property('payload',[]);
         }
         catch(err) { done(err);
         }
       });
-      n1.receive({ invalid:true, payload: 12.345 });
-      n1.receive({ invalid:true, payload: -12.345 });
-      n1.receive({ invalid:true, payload: 0 });
-      n1.receive({ payload: 255 });
+      await delay(750);
+      n1.receive({ invalid:true, topic:"s", payload: 12.345 });
+      n1.receive({ invalid:true, topic:"s", payload: -12.345 });
+      n1.receive({ invalid:true, topic:"s", payload: 0 });
+      await delay(1750);
+      done();
     });
   });
-
+/*
   it('should forward NaN data as it is', function (done) {
     const numbers = [undefined,"FooBar",NaN,{}];
     var flow = [{ id: "n1", type: "formatNumber", name: "test", wires: [["n2"]] },

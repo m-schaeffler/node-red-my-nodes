e456e700ec8ae12356403ba68b9bbe502bb54b1a
Mathias Schäffler <mschaeffler@gmx.de>
2024-05-08 15:13:17 +0200

    coillectChart: new test case

--- node-red-format/test/collect_chart_spec.js
+++ node-red-format/test/collect_chart_spec.js
@@ -662,6 +662,68 @@ describe( 'collect_chart Node', function () {
     });
   });
 
+  it('should reload from context', function (done) {
+    this.timeout( 10000 );
+    const topics = ["old","old2"];
+    var flow = [{ id: "n1", type: "collectChart", contextStore:"memoryOnly", cycleJitter: 0, cyclic: 1, name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+     initContext(async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg);
+        try {
+          c++;
+          switch( c )
+          {
+            case 1:
+              msg.should.have.property('init',true);
+              msg.should.have.property('payload').which.is.an.Array().of.length(topics.length);
+              for( const i in topics )
+              {
+                msg.payload[i].should.match({c:topics[i]});
+              }
+              break;
+            case 2:
+              break;
+            default:
+              done("too much output messages");
+          }
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('contextStore', 'memoryOnly');
+        n1.context().set("data", [{c:'old'},{c:'old2'}], "memoryOnly");
+        await delay(750);
+        c.should.match(1);
+        n1.receive({ topic:"end", payload: 255 });
+        await delay(2750);
+        c.should.match(2);
+        should.not.exist( n1.context().get("last") );
+        const q = n1.context().get("data", "memoryOnly");
+        q.should.be.an.Array().of.length(topics.length+1);
+        for(const i in topics)
+        {
+          const v = q[i];
+          v.should.be.a.Object();
+          v.should.have.a.property('c',topics[i]);
+          v.should.not.have.a.property('t');
+          v.should.not.have.a.property('v');
+        }
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+     });
+    });
+  });
+
   it('should fix changed topics, to less data points', function (done) {
     this.timeout( 10000 );
     const topics = ["a","b","c","d"];

14f4812dcd3d89c0f61adc94ec96d5b236b8cd07
Mathias Schäffler <mathias@fam-schaeffler.de>
2024-11-03 15:28:12 +0100

    msg-resend: reset with byTopic

--- node-red-msg-resend/test/msg_resend_spec.js
+++ node-red-msg-resend/test/msg_resend_spec.js
@@ -44,13 +44,16 @@ describe( 'msg-resend Node', function () {
   });
 
   function checkData(data,topic) {
-      data.should.be.a.Object();
-      data[topic].should.be.a.Object();
-      for( const i in data )
-      {
-          data[i].should.have.a.property('counter',data[i].maxCount);
-          data[i].should.have.a.property('timer',null);
-      }
+      data.should.have.a.property(topic).which.is.a.Object();
+      data[topic].should.have.a.property('counter',data[topic].maxCount);
+      data[topic].should.have.a.property('timer',null);
+      return data[topic];
+  }
+
+  function checkDataWithoutCounter(data,topic) {
+      data.should.have.a.property(topic).which.is.a.Object();
+      data[topic].should.not.have.a.property('counter');
+      data[topic].should.have.a.property('timer',null);
       return data[topic];
   }
 
@@ -531,6 +534,100 @@ describe( 'msg-resend Node', function () {
         await delay(175);
         checkData( n1.context().get("data"), "all_topics" );
         c.should.match(23);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+     });
+    });
+  });
+
+  it('should be stopped by msg.reset with topic', function (done) {
+    this.timeout( 5000 );
+    var flow = [{ id: "n1", type: "msg-resend2", name: "test", interval:50, intervalUnit:"msecs", maximum:0, wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+     initContext(async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        //console.log(msg);
+        try {
+          msg.should.have.a.property('topic','t');
+          msg.should.have.a.property('payload',1);
+          msg.should.not.have.a.property('counter');
+          msg.should.not.have.a.property('max');
+        }
+        catch(err) {
+          done(err);
+        }
+        c++;
+      });
+      try {
+        n1.should.have.a.property('interval', 50);
+        n1.should.have.a.property('maxCount', 0);
+        n1.should.have.a.property('byTopic', false);
+        await delay(500);
+        should.exist( n1.context().get("data") );
+        c.should.match(0);
+        n1.receive({ topic: "t", payload: 1 });
+        await delay(25);
+        c.should.match(1);
+        await delay(500);
+        c.should.match(11);
+        n1.receive({ topic: "t", reset: true });
+        await delay(175);
+        checkDataWithoutCounter( n1.context().get("data"), "all_topics" );
+        c.should.match(11);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+     });
+    });
+  });
+
+  it('should be stopped by msg.reset without topic', function (done) {
+    this.timeout( 5000 );
+    var flow = [{ id: "n1", type: "msg-resend2", name: "test", interval:50, intervalUnit:"msecs", maximum:0, wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+     initContext(async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        //console.log(msg);
+        try {
+          msg.should.have.a.property('topic','t');
+          msg.should.have.a.property('payload',1);
+          msg.should.not.have.a.property('counter');
+          msg.should.not.have.a.property('max');
+        }
+        catch(err) {
+          done(err);
+        }
+        c++;
+      });
+      try {
+        n1.should.have.a.property('interval', 50);
+        n1.should.have.a.property('maxCount', 0);
+        n1.should.have.a.property('byTopic', false);
+        await delay(500);
+        should.exist( n1.context().get("data") );
+        c.should.match(0);
+        n1.receive({ topic: "t", payload: 1 });
+        await delay(25);
+        c.should.match(1);
+        await delay(500);
+        c.should.match(11);
+        n1.receive({ reset: true });
+        await delay(175);
+        checkDataWithoutCounter( n1.context().get("data"), "all_topics" );
+        c.should.match(11);
         done();
       }
       catch(err) {
--- node-red-msg-resend/test/msg_resend_topic_spec.js
+++ node-red-msg-resend/test/msg_resend_topic_spec.js
@@ -48,13 +48,16 @@ describe( 'msg-resend Node, byTopic', function () {
   });
 
   function checkData(data,topic) {
-      data.should.be.a.Object();
-      data[topic].should.be.a.Object();
-      for( const i in data )
-      {
-          data[i].should.have.a.property('counter',data[i].maxCount);
-          data[i].should.have.a.property('timer',null);
-      }
+      data.should.have.a.property(topic).which.is.a.Object();
+      data[topic].should.have.a.property('counter',data[topic].maxCount);
+      data[topic].should.have.a.property('timer',null);
+      return data[topic];
+  }
+
+  function checkDataWithoutCounter(data,topic) {
+      data.should.have.a.property(topic).which.is.a.Object();
+      data[topic].should.not.have.a.property('counter');
+      data[topic].should.have.a.property('timer',null);
       return data[topic];
   }
 
@@ -487,6 +490,81 @@ describe( 'msg-resend Node, byTopic', function () {
         await delay(175);
         checkData( n1.context().get("data"), "t" );
         c.should.match(23);
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+     });
+    });
+  });
+
+  it('should be stopped by msg.reset', function (done) {
+    this.timeout( 5000 );
+    var flow = [{ id: "n1", type: "msg-resend2", name: "test", bytopic:true, interval:50, intervalUnit:"msecs", maximum:0, wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, function () {
+     initContext(async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      var phase = 0;
+      n2.on("input", function (msg) {
+        //console.log(msg);
+        try {
+          switch( phase )
+          {
+            case 0:
+              msg.should.have.a.property('topic',topics2[c%3]);
+              msg.should.have.a.property('payload',c%3+1);
+              break;
+            case 1:
+              msg.should.have.a.property('topic',topics2[c%2]);
+              msg.should.have.a.property('payload',c%2+1);
+              break;
+            case 2:
+              done("no messages in 3rd phase");
+              break;
+            default:
+              done("wrong phase");
+          }
+          msg.should.not.have.a.property('counter');
+          msg.should.not.have.a.property('max');
+        }
+        catch(err) {
+          done(err);
+        }
+        c++;
+      });
+      try {
+        n1.should.have.a.property('interval', 50);
+        n1.should.have.a.property('maxCount', 0);
+        n1.should.have.a.property('byTopic', true);
+        await delay(500);
+        should.exist( n1.context().get("data") );
+        c.should.match(0);
+        for(const i in topics2)
+        {
+          n1.receive({ topic: topics2[i], payload: Number(i)+1 });
+        }
+        await delay(25);
+        c.should.match(topics2.length);
+        await delay(500);
+        c.should.match(11*topics2.length);
+        n1.receive({ topic: "v", reset: true });
+        phase++;
+        c=0;
+        await delay(500);
+        checkDataWithoutCounter( n1.context().get("data"), "v" );
+        c.should.match(20);
+        n1.receive({ reset: true });
+        phase++;
+        c=0;
+        await delay(500);
+        checkDataWithoutCounter( n1.context().get("data"), "t" );
+        checkDataWithoutCounter( n1.context().get("data"), "u" );
+        checkDataWithoutCounter( n1.context().get("data"), "v" );
+        c.should.match(0);
         done();
       }
       catch(err) {

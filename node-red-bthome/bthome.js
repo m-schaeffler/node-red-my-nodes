194e1324c09bbbc917929512ac5931007cd7901b
Mathias Schäffler <mathias@fam-schaeffler.de>
2025-04-17 19:54:24 +0200

    bthome: more work

--- node-red-bthome/bthome.js
+++ node-red-bthome/bthome.js
@@ -1,3 +1,60 @@
+class Rawdata {
+    constructor(input)
+    {
+        this._data = input;
+    }
+    _utoi(num,bits)
+    {
+        const mask = 1 << ( bits - 1 );
+        return num & mask ? num - ( 1 << bits ) : num;
+    }
+    getUInt8()
+    {
+        return this._data.shift();
+    }
+    getInt8()
+    {
+        return this._utoi( this.getUInt8(), 8 );
+    }
+    getUInt16()
+    {
+        const a = this._data.shift();
+        const b = this._data.shift();
+        return 0xffff & ( ( b << 8 ) | a );
+    }
+    getInt16()
+    {
+        return this._utoi( this.getUInt16(), 16 );
+    }
+    getUInt24()
+    {
+        const a = this._data.shift();
+        const b = this._data.shift();
+        const c = this._data.shift();
+        return 0xffffff & ( ( c << 16 ) | ( b << 8 ) | a );
+    }
+    getInt24()
+    {
+        return this._utoi( this.getUInt24(), 24 );
+    }
+    getUInt32()
+    {
+        const a = this._data.shift();
+        const b = this._data.shift();
+        const c = this._data.shift();
+        const d = this._data.shift();
+        return 0xffffffff & ( ( d << 24 ) | ( c << 16 ) | ( b << 8 ) | a );
+    }
+    getInt32()
+    {
+        return this._utoi( this.getUInt32(), 32 );
+    }
+    getEnum(values)
+    {
+        return values[this.getUInt8()] ?? "";
+    }
+}
+
 module.exports = function(RED) {
 
     function BtHomeNode(config) {
@@ -5,12 +62,13 @@ module.exports = function(RED) {
         var node = this;
         this.flowcontext  = this.context().flow;
         this.devices      = JSON.parse( config.devices ?? "{}" );
+        this.contextVar   = config.contextVar ?? "bthome";
         this.contextStore = config.contextStore ?? "none";
         this.data         = {};
         node.status( "" );
         if( node.contextStore !== "none" )
         {
-            node.flowcontext.get( "bthome", node.contextStore, function(err,value)
+            node.flowcontext.get( node.contextVar, node.contextStore, function(err,value)
             {
                 if( err )
                 {
@@ -26,13 +84,40 @@ module.exports = function(RED) {
                 }
             } );
         }
+        for( const mac in node.devices )
+        {
+            const device = node.devices[mac];
+            switch( typeof device.key )
+            {
+                case "undefined":
+                    break;
+                case "string":
+                    if( device.key.length == 32 )
+                    {
+                        const buf = Buffer.alloc( 16 );
+                        for( let i=0; i<16 ; i++ )
+                        {
+                            buf[i] = Number.parseInt( device.key.slice( i*2, i*2 + 2 ), 16 );
+                        }
+                        device.key = buf;
+                    }
+                    else
+                    {
+                        device.key = Buffer.alloc( 16 );
+                        console.log( mac + " keylength must be 16 bytes" );
+                    }
+                    break;
+                default:
+                    device.key = Buffer.from( device.key );
+            }
+        }
 
         node.on('input', function(msg,send,done) {
             console.log(node.data)
             node.data[msg.topic] = msg.payload;
             if( node.contextStore !== "none" )
             {
-                node.flowcontext.set( "bthome", node.data, node.contextStore );
+                node.flowcontext.set( node.contextVar, node.data, node.contextStore );
             }
             send( {topic:"bthome",payload:node.data} )
             done();
--- node-red-bthome/test/bthome_spec.js
+++ node-red-bthome/test/bthome_spec.js
@@ -27,13 +27,63 @@ describe( 'bthome Node', function () {
   });
 
   it('should be loaded', function (done) {
-    var flow = [{ id: "n1", type: "bthome", name: "test", z:"flow" }];
+    let flow = [{ id: "n1", type: "bthome", name: "test", z:"flow" }];
     helper.load(node, flow, async function () {
-      var n1 = helper.getNode("n1");
+      let n1 = helper.getNode("n1");
       try {
         n1.should.have.a.property('name', 'test');
         n1.should.have.a.property('devices', {});
+        n1.should.have.a.property('contextVar', "bthome");
         n1.should.have.a.property('contextStore', "none");
+        await delay(50);
+        n1.should.have.a.property('data', {} );
+        should.not.exist( n1.context().flow.get("bthome") );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should process device data', function (done) {
+    const devicesJson = '{ \
+        "11:22:33:44:55:66": { "topic": "device1" }, \
+        "00:00:00:00:00:11": { "topic": "device2", "key": "" }, \
+        "0f:0e:0d:0c:0b:0a": { "topic": "device3", "key": [] }, \
+        "00:00:00:00:00:22": { "topic": "device4", "key": "--------------------------------" }, \
+        "00:01:02:03:04:05": { "topic": "device5", "key": "00112233445566778899AABBCCDDEEFF" }, \
+        "00:10:20:30:40:50": { "topic": "device6", "key": [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16] } }';
+    let flow = [{ id: "n1", type: "bthome", name: "test", devices:devicesJson, z:"flow" }];
+    helper.load(node, flow, async function () {
+      let n1 = helper.getNode("n1");
+      try {
+        n1.should.have.a.property('name', 'test');
+        n1.should.have.a.property('devices');
+        n1.should.have.a.property('contextVar', "bthome");
+        n1.should.have.a.property('contextStore', "none");
+        let d = n1.devices;
+        d.should.be.an.Object();
+        function testDevice(dev,topic,key)
+        {
+          d.should.have.a.property(dev);
+          d[dev].should.be.an.Object();
+          d[dev].should.have.a.property("topic",topic);
+          if( key )
+          {
+            d[dev].should.have.a.property("key",key);
+          }
+          else
+          {
+            d[dev].should.not.have.a.property("key");
+          }
+        }
+        testDevice("11:22:33:44:55:66","device1");
+        testDevice("00:00:00:00:00:11","device2",Buffer.alloc(16));
+        testDevice("0f:0e:0d:0c:0b:0a","device3",Buffer.alloc(0));
+        testDevice("00:00:00:00:00:22","device4",Buffer.alloc(16));
+        testDevice("00:01:02:03:04:05","device5",Buffer.from([0x00,0x11,0x22,0x33,0x44,0x55,0x66,0x77,0x88,0x99,0xAA,0xBB,0xCC,0xDD,0xEE,0xFF]));
+        testDevice("00:10:20:30:40:50","device6",Buffer.from([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]));
         await delay(50);
         n1.should.have.a.property('data', {} );
         should.not.exist( n1.context().flow.get("bthome") );

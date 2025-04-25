a004c0ac8289b85c7d006339da7201e548357cef
Mathias Schäffler <mathias@fam-schaeffler.de>
2025-04-25 09:03:45 +0200

    bthome: UT for encrypt helper

--- node-red-bthome/test/encrypt_spec.js
+++ node-red-bthome/test/encrypt_spec.js
@@ -6,12 +6,12 @@ describe( 'encrypt helper', function () {
 
   it('should encrypt bthome messages', function (done) {
     try {
-      console.log( Encrypt.encryptBthome(
+      Encrypt.encryptBthome(
         [69,0,128,5,3,2,1,0x2D,1,0x3F,60,0],
         '00:10:20:30:40:50',
         0x00112233,
         '00112233445566778899AABBCCDDEEFF'
-     ) );
+      ).should.match( [69,185,49,198,170,133,200,48,253,111,234,66,0,17,34,51,42,184,90,0] );
       done();
     }
     catch(err) {

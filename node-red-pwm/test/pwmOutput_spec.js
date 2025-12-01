120f7c1a1efd21c367fb4e1fa6c497c19bf37ee5
Mathias Schäffler <mathias@fam-schaeffler.de>
2025-12-01 08:29:06 +0100

    pwm-output: UTs

--- node-red-pwm/test/pwmOutput_spec.js
+++ node-red-pwm/test/pwmOutput_spec.js
@@ -49,11 +49,12 @@ describe( 'pmw_output Node', function () {
       var n1 = helper.getNode("n1");
       var c = 0;
       n2.on("input", function (msg) {
-        console.log(msg);
+        //console.log(msg);
         try {
           c++;
           msg.should.have.property('topic',"input"+c);
           msg.should.have.property('payload',!Boolean(c%2));
+          msg.should.have.property('pwm');
         }
         catch(err) {
           done(err);
@@ -116,6 +117,14 @@ describe( 'pmw_output Node', function () {
               const delta = Date.now() - start;
               delta.should.be.approximately( c<6 ? 500*0.25 : 50, 10 );
           }
+          if( c == 1 || c == 6 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
+          }
         }
         catch(err) {
           done(err);
@@ -173,6 +182,14 @@ describe( 'pmw_output Node', function () {
               const delta = Date.now() - start;
               delta.should.be.approximately( 500*0.25, 10 );
           }
+          if( c == 1 || c == 6 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
+          }
         }
         catch(err) {
           done(err);
@@ -211,7 +228,7 @@ describe( 'pmw_output Node', function () {
       var c = 0;
       var start;
       n2.on("input", function (msg) {
-        console.log(msg);
+        //console.log(msg);
         try {
           c++;
           msg.should.have.property('topic','PWM');
@@ -229,6 +246,14 @@ describe( 'pmw_output Node', function () {
           {
               const delta = Date.now() - start;
               delta.should.be.approximately( 500*0.25, 10 );
+          }
+          if( c <= 2 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
           }
         }
         catch(err) {
@@ -292,6 +317,14 @@ describe( 'pmw_output Node', function () {
               const delta = Date.now() - start;
               delta.should.be.approximately( c<6 ? 500*0.5 : 50, 10 );
           }
+          if( c == 1 || c == 6 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
+          }
         }
         catch(err) {
           done(err);
@@ -348,6 +381,14 @@ describe( 'pmw_output Node', function () {
           {
               const delta = Date.now() - start;
               delta.should.be.approximately( c<6 ? 500*0.75 : 50, 10 );
+          }
+          if( c == 1 || c == 6 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
           }
         }
         catch(err) {
@@ -406,6 +447,14 @@ describe( 'pmw_output Node', function () {
               const delta = Date.now() - start;
               delta.should.be.approximately( 500*0.01, 10 );
           }
+          if( c == 1 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
+          }
         }
         catch(err) {
           done(err);
@@ -463,6 +512,14 @@ describe( 'pmw_output Node', function () {
               const delta = Date.now() - start;
               delta.should.be.approximately( c<6 ? 500*0.99 : 50, 10 );
           }
+          if( c == 1 || c == 6 )
+          {
+               msg.should.have.property('pwm');
+          }
+          else
+          {
+               msg.should.not.have.property('pwm');
+          }
         }
         catch(err) {
           done(err);
@@ -491,158 +548,6 @@ describe( 'pmw_output Node', function () {
     });
   });
 
-/*
-  it('should forward numbers rounded to two digits and changed decimal', function (done) {
-    const numbers = [-1,0,1,12.345,-12.345,"-1","0","1","34.5","-34.5",true,false];
-    var flow = [{ id: "n1", type: "formatNumber", decimal: ",", digits: "2", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',Number(numbers[c]).toFixed(2).replace('.',','));
-          ++c;
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('decimal', ",");
-        n1.should.have.a.property('digits', 2);
-        await delay(50);
-        for( const i of numbers )
-        {
-          n1.receive({ payload: i });
-          await delay(50);
-        }
-        c.should.match( numbers.length );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-
-  it('should forward numbers with an added unit', function (done) {
-    const numbers = [-1,0,1,12.345,-12.345,"-1","0","1","34.5","-34.5",true,false];
-    var flow = [{ id: "n1", type: "formatNumber", unit: "VAr", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',Number(numbers[c]).toFixed(0)+'\u202FVAr');
-          ++c;
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('unit', '\u202FVAr');
-        await delay(50);
-        for( const i of numbers )
-        {
-          n1.receive({ payload: i });
-          await delay(50);
-        }
-        c.should.match( numbers.length );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-
-  it('should forward numbers without grouping', function (done) {
-    const numbers = [-12345,123,1234,12345,123456,1234567,12345678,1234.1,1233.9,0,1,-1];
-    const results = ["-12345","123","1234","12345","123456","1234567","12345678","1234","1234","0","1","-1"];
-    var flow = [{ id: "n1", type: "formatNumber", grouping: "", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',results[c]);
-          ++c;
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('grouping', "");
-        await delay(50);
-        for( const i of numbers )
-        {
-          n1.receive({ payload: i });
-          await delay(50);
-        }
-        c.should.match( results.length );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-
-  it('should forward numbers with grouping', function (done) {
-    const numbers = [-12345,123,1234,12345,123456,1234567,12345678,1234.1,1233.9,0,1,-1];
-    const results = ["-12'345","123","1'234","12'345","123'456","1'234'567","12'345'678","1'234","1'234","0","1","-1"];
-    var flow = [{ id: "n1", type: "formatNumber", grouping: "'", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        //console.log(msg.payload);
-        try {
-          msg.should.have.property('payload',results[c]);
-          ++c;
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('grouping', "$1'");
-        await delay(50);
-        for( const i of numbers )
-        {
-          n1.receive({ payload: i });
-          await delay(50);
-        }
-        c.should.match( results.length );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-*/
   it('should not use invalid data', function (done) {
     var flow = [{ id: "n1", type: "pwmOutput", cyclicTime:"0.1", name: "test", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
@@ -686,118 +591,7 @@ describe( 'pmw_output Node', function () {
       }
     });
   });
-/*
-  it('should forward NaN data as it is', function (done) {
-    const numbers = [undefined,"FooBar",NaN,{}];
-    var flow = [{ id: "n1", type: "formatNumber", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        try {
-          msg.should.have.a.property('payload',numbers[c]);
-          ++c;
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try{
-        await delay(50);
-        for( const i of numbers )
-        {
-          n1.receive({ payload: i });
-          await delay(50);
-        }
-        c.should.match( numbers.length );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
 
-  it('should not filter data', function (done) {
-    var flow = [{ id: "n1", type: "formatNumber", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        c++;
-        try {
-          msg.should.have.a.property('payload',c===4?"255":"2");
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('filter', false);
-        await delay(50);
-        n1.receive({ payload: 2 });
-        await delay(50);
-        n1.receive({ payload: 2 });
-        await delay(50);
-        n1.receive({ payload: 2 });
-        await delay(50);
-        n1.receive({ payload: 255 });
-        await delay(50);
-        c.should.match( 4 );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-
-  it('should filter data', function (done) {
-    var flow = [{ id: "n1", type: "formatNumber", name: "test", filter:true, wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        c++;
-        try {
-          msg.should.have.a.property('payload',c===2?"255":"2");
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('filter', true);
-        await delay(50);
-        n1.receive({ payload: 2 });
-        await delay(50);
-        n1.receive({ payload: 2 });
-        await delay(50);
-        n1.receive({ payload: 2 });
-        await delay(50);
-        n1.receive({ payload: 255 });
-        await delay(50);
-        c.should.match( 2 );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-*/
   it('should work with objects', function (done) {
     var flow = [{ id: "n1", type: "pwmOutput", name: "test", property:"payload.value", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];

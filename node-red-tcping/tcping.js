6b5827401505deb35c47b261ae13a68a6cecb9b4
Mathias Schäffler <mathias@fam-schaeffler.de>
2026-03-18 12:40:13 +0100

    tcping: first version

--- node-red-tcping/tcping.js
+++ node-red-tcping/tcping.js
@@ -1,27 +1,95 @@
+const Net = require( 'net' );
+
 module.exports = function(RED) {
 
     function TcPingNode(config) {
         RED.nodes.createNode(this,config);
         var node = this;
+        this.host   = config.host ?? "";
+        this.port   = Number( config.port ?? 80 );
+        this.family = Number( config.family ?? 0 );
+        this.openSockets = [];
         node.status( "" );
 
         node.on('input', async function(msg,send,done) {
-            try
-            {
-                done();
-            }
-            catch( e )
-            {
-                console.log(e);
-                //node.stats.exception++;
-                node.status( {
-                    fill:  "red",
-                    shape: "dot",
-                    text:  e.name
+                const socket = new Net.Socket();
+                node.openSockets.push( socket );
+                msg.ping = {
+                    host:   node.host,
+                    port:   node.port,
+                    family: null,
+                    ip:     null
+                };
+
+                function sendResult(result)
+                {
+                    msg.payload = result;
+                    send( msg );
+                    const index = node.openSockets.indexOf( socket );
+                    if( index > -1 )
+                    {
+                        node.openSockets.splice( index, 1 );
+                    }
+                    done();
+                }
+
+                function socketConnectionAttempt(ip,port,family)
+                {
+                    console.log("socketConnectionAttempt",ip,port,family)
+                    msg.ping.ip     = ip;
+                    msg.ping.family = family;
+                }
+
+                function socketConnect()
+                {
+                    console.log("socketConnect")
+                    socket.end();
+                    sendResult( Date.now() - start );
+                }
+
+                function socketError(event)
+                {
+                    console.log("socketError",event.message)
+                    msg.ping.error = event.message;
+                    sendResult( false );
+                }
+
+                socket.addListener( "connectionAttempt", socketConnectionAttempt );
+                socket.addListener( "connectionAttemptFailed", socketConnectionAttemptFailed );
+                socket.addListener( "connectionAttemptTimeout", socketConnectionAttemptTimeout );
+                socket.addListener( "connect", socketConnect );
+                socket.addListener( "close", socketClose );
+                socket.addListener( "error", socketError );
+                const start  = Date.now();
+                socket.connect( {
+                    family: node.family,
+                    host:   msg.ping.host,
+                    port:   msg.ping.port
                 } );
-                done( e );
+        });
+
+        node.on('close', function() {
+            for(const i of node.openSockets )
+            {
+                console.log("close",i.connecting)
+                i.destroy();
             }
         });
+    }
+
+    function socketConnectionAttemptFailed(event)
+    {
+        console.log("socketConnectionAttemptFailed",event)
+    }
+
+    function socketConnectionAttemptTimeout(event)
+    {
+        console.log("socketConnectionAttemptTimeout",event)
+    }
+
+    function socketClose(hadError)
+    {
+        console.log("socketClose",hadError)
     }
 
     RED.nodes.registerType( "tcPing", TcPingNode );
--- node-red-tcping/test/tcping_spec.js
+++ node-red-tcping/test/tcping_spec.js
@@ -1,6 +1,7 @@
 var should = require("should");
 var helper = require("node-red-node-test-helper");
 var node   = require("../tcping.js");
+var net    = require("net");
 
 function delay(ms) {
   return new Promise((resolve) => {
@@ -27,9 +28,9 @@ describe( 'tcPing Node', function () {
       var n1 = helper.getNode("n1");
       try {
         n1.should.have.a.property('name', 'test');
-        //n1.should.have.a.property('fems', null);
-        //n1.should.have.a.property('topic', '');
-        //n1.should.have.a.property('complete', false);
+        n1.should.have.a.property('host', '');
+        n1.should.have.a.property('port', 80);
+        n1.should.have.a.property('family', 0);
         await delay(50);
         n1.warn.should.have.callCount(0);
         n1.error.should.have.callCount(0);
@@ -41,22 +42,36 @@ describe( 'tcPing Node', function () {
     });
   });
 
-/*
-  it('should make a simple request', function (done) {
+ it('should ping a fixed URL', function (done) {
     var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"fems.lan", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
+                { id: "n1", type: "tcPing", host: "www.google.com", port:"443", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
       var c = 0;
       n2.on("input", function (msg) {
-        //console.log(msg);
+        console.log(msg);
         try {
-          msg.should.have.property('topic',"_meta/Version");
-          msg.should.have.property('payload').which.is.a.String();
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload').which.is.within(1,150);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','www.google.com');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family').which.is.a.Number();
+          msg.ping.should.have.property('ip');
+          switch( msg.ping.family )
+          {
+              case 4:
+                  net.isIPv4( msg.ping.ip ).should.match( true );
+                  break;
+              case 6:
+                  net.isIPv6( msg.ping.ip ).should.match( true );
+                  break;
+              default:
+                  done("invalid family");
+          }
+          msg.ping.should.not.have.property('error');
           ++c;
         }
         catch(err) {
@@ -65,17 +80,15 @@ describe( 'tcPing Node', function () {
       });
       try{
         n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('topic', '');
-        n1.should.have.a.property('complete', false);
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
+        n1.should.have.a.property('host','www.google.com');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 0);
         await delay(50);
-        n1.receive({ topic:"_meta/Version" });
-        await delay(200);
+        n1.receive({ topic:"foobar" });
+        await delay(300);
         n1.warn.should.have.callCount(0);
         n1.error.should.have.callCount(0);
         c.should.match( 1 );
-        n1.should.have.a.property('stats',{ok:1,error:0,exception:0});
         done();
       }
       catch(err) {
@@ -84,21 +97,25 @@ describe( 'tcPing Node', function () {
     });
   });
 
-  it('should make a wildcard request', function (done) {
+  it('should ping an invalid URL', function (done) {
     var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"fems.lan", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
+                { id: "n1", type: "tcPing", host: "foo:bar", port:"443", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
       var c = 0;
       n2.on("input", function (msg) {
         //console.log(msg);
         try {
-          msg.should.have.property('topic',"_meta/.*");
-          msg.should.have.property('payload').which.is.an.Object();
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload',false);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','foo:bar');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family',null);
+          msg.ping.should.have.property('ip',null);
+          msg.ping.should.have.property('error');
           ++c;
         }
         catch(err) {
@@ -107,17 +124,15 @@ describe( 'tcPing Node', function () {
       });
       try{
         n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('topic', '');
-        n1.should.have.a.property('complete', false);
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
+        n1.should.have.a.property('host','foo:bar');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 0);
         await delay(50);
-        n1.receive({ topic:"_meta/.*" });
-        await delay(200);
+        n1.receive({ topic:"foobar" });
+        await delay(50);
         n1.warn.should.have.callCount(0);
         n1.error.should.have.callCount(0);
         c.should.match( 1 );
-        n1.should.have.a.property('stats',{ok:1,error:0,exception:0});
         done();
       }
       catch(err) {
@@ -126,27 +141,25 @@ describe( 'tcPing Node', function () {
     });
   });
 
-  it('should make a predefined request with complete option', function (done) {
+  it('should ping an invalid Address', function (done) {
     var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", topic: "_meta/Version", complete:true, name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"fems.lan", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
+                { id: "n1", type: "tcPing", host: "foo.bar", port:"443", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
       var c = 0;
       n2.on("input", function (msg) {
         //console.log(msg);
         try {
-          msg.should.have.property('topic',"foo/bar");
-          msg.should.have.property('payload').which.is.an.Object();
-          msg.payload.should.have.property('address','_meta/Version');
-          msg.payload.should.have.property('type','STRING');
-          msg.payload.should.have.property('accessMode','RO');
-          msg.payload.should.have.property('text');
-          msg.payload.should.have.property('unit','');
-          msg.payload.should.have.property('value');
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload',false);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','foo.bar');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family',null);
+          msg.ping.should.have.property('ip',null);
+          msg.ping.should.have.property('error');
           ++c;
         }
         catch(err) {
@@ -155,17 +168,15 @@ describe( 'tcPing Node', function () {
       });
       try{
         n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('topic', '_meta/Version');
-        n1.should.have.a.property('complete', true);
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
+        n1.should.have.a.property('host','foo.bar');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 0);
         await delay(50);
-        n1.receive({ topic:"foo/bar" });
-        await delay(200);
+        n1.receive({ topic:"foobar" });
+        await delay(100);
         n1.warn.should.have.callCount(0);
         n1.error.should.have.callCount(0);
         c.should.match( 1 );
-        n1.should.have.a.property('stats',{ok:1,error:0,exception:0});
         done();
       }
       catch(err) {
@@ -174,31 +185,42 @@ describe( 'tcPing Node', function () {
     });
   });
 
-  it('should handle invalid requests', function (done) {
+  it('should ping an invalid IP', function (done) {
     var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"fems.lan", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
+                { id: "n1", type: "tcPing", host: "192.168.254.254", port:"443", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
       var c = 0;
       n2.on("input", function (msg) {
-        console.log(msg);
+        //console.log(msg);
+        try {
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload',false);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','192.168.254.254');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family',4);
+          msg.ping.should.have.property('ip','192.168.254.254');
+          msg.ping.should.have.property('error');
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
       });
       try{
         n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('topic', '');
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
+        n1.should.have.a.property('host','192.168.254.254');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 0);
         await delay(50);
-        n1.receive({ topic:"foo/bar" });
-        await delay(200);
+        n1.receive({ topic:"foobar" });
+        await delay(50);
         n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(1);
-        c.should.match( 0 );
-        n1.should.have.a.property('stats',{ok:0,error:1,exception:0});
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
         done();
       }
       catch(err) {
@@ -207,30 +229,43 @@ describe( 'tcPing Node', function () {
     });
   });
 
-  it('should handle invalid URLs', function (done) {
+  it('should timeout', function (done) {
+    this.timeout( 5000 );
     var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"foobar:lan", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
+                { id: "n1", type: "tcPing", host: "192.168.3.254", port:"443", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
       var c = 0;
       n2.on("input", function (msg) {
-        console.log(msg);
+        //console.log(msg);
+        try {
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload',false);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','192.168.3.254');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family',4);
+          msg.ping.should.have.property('ip','192.168.3.254');
+          msg.ping.should.have.property('error');
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
       });
       try{
         n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
+        n1.should.have.a.property('host','192.168.3.254');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 0);
         await delay(50);
-        n1.receive({ topic:"foo/bar" });
-        await delay(200);
+        n1.receive({ topic:"foobar" });
+        await delay(4000);
         n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(1);
-        c.should.match( 0 );
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:1});
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
         done();
       }
       catch(err) {
@@ -239,30 +274,29 @@ describe( 'tcPing Node', function () {
     });
   });
 
-  it('should handle invalid addresses', function (done) {
+  it('should be closed while connecting', function (done) {
     var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"foobar.lan", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
+                { id: "n1", type: "tcPing", host: "192.168.3.254", port:"443", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
       var c = 0;
       n2.on("input", function (msg) {
         console.log(msg);
+        ++c;
       });
       try{
         n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
+        n1.should.have.a.property('host','192.168.3.254');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 0);
         await delay(50);
-        n1.receive({ topic:"foo/bar" });
-        await delay(200);
+        n1.receive({ topic:"foobar" });
+        await delay(50);
         n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(1);
+        n1.error.should.have.callCount(0);
         c.should.match( 0 );
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:1});
         done();
       }
       catch(err) {
@@ -271,36 +305,4 @@ describe( 'tcPing Node', function () {
     });
   });
 
-  it('should handle invalid IPs', function (done) {
-    var flow = [{ id: 'flow', type: 'tab' },
-                { id: "n1", type: "feneconHttpGet", fems: "nf", name: "test", wires: [["n2"]], z: "flow" },
-                { id: "n2", type: "helper", z: "flow" },
-                { id: "nf", type: "feneconFems", hostname:"192.168.254.254", name:"TestFems", z: "flow" }];
-    helper.load([node,nodeFems], flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var nf = helper.getNode("nf");
-      var c = 0;
-      n2.on("input", function (msg) {
-        console.log(msg);
-      });
-      try{
-        n1.should.have.a.property('name', 'test');
-        n1.should.have.a.property('fems').which.is.an.Object();
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:0});
-        await delay(50);
-        n1.receive({ topic:"foo/bar" });
-        await delay(200);
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(1);
-        c.should.match( 0 );
-        n1.should.have.a.property('stats',{ok:0,error:0,exception:1});
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-*/
 });

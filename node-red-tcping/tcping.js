894dbda5229a97227963b54f587ebf04cd14b1dd
Mathias Schäffler <mathias@fam-schaeffler.de>
2026-03-18 16:32:18 +0100

    tcping: paylaod implemented

--- node-red-tcping/tcping.js
+++ node-red-tcping/tcping.js
@@ -12,60 +12,113 @@ module.exports = function(RED) {
         node.status( "" );
 
         node.on('input', async function(msg,send,done) {
-                const socket = new Net.Socket();
-                node.openSockets.push( socket );
-                msg.ping = {
-                    host:   node.host,
-                    port:   node.port,
-                    family: null,
-                    ip:     null
-                };
+            const socket = new Net.Socket();
+            let   family;
+            msg.ping = {
+                family: null,
+                ip:     null
+            };
+            switch( typeof msg.payload )
+            {
+                case "undefined":
+                    msg.ping.host = node.host;
+                    msg.ping.port = node.port;
+                    family        = node.family;
+                    break;
+                case "string":
+                  {
+                    const help = msg.payload.split( ':' );
+                    msg.ping.host = help[0];
+                    msg.ping.port = Number( help?.[1] ?? node.port );
+                    family        = node.family;
+                  }
+                    break;
+                case "object":
+                    msg.ping.host = msg.payload.host ?? node.host;
+                    msg.ping.port = Number( msg.payload.port ?? node.port );
+                    family        = Number( msg.payload.family ?? node.family );
+                    break;
+                default:
+                    done( "unknown type of payload: "+msg.payload );
+                    return;
+            }
+            if( ! msg.ping.host )
+            {
+                done( "invalid host: "+msg.ping.host );
+                return;
+            }
+            if( ! ( msg.ping.port > 0 && msg.ping.port <= 0xFFFF && Number.isInteger( msg.ping.port ) ) )
+            {
+                //console.log("invalid port: "+msg.ping.port)
+                done( "invalid port: "+msg.ping.port );
+                return;
+            }
+            if( ! ( family === 0 || family === 4 || family === 6 ) )
+            {
+                //console.log("invalid family: "+family)
+                done( "invalid family: "+family );
+                return;
+            }
 
-                function sendResult(result)
+            function sendResult(result,color)
+            {
+                msg.payload = result;
+                send( msg );
+                const index = node.openSockets.indexOf( socket );
+                if( index > -1 )
                 {
-                    msg.payload = result;
-                    send( msg );
-                    const index = node.openSockets.indexOf( socket );
-                    if( index > -1 )
-                    {
-                        node.openSockets.splice( index, 1 );
-                    }
-                    done();
+                    node.openSockets.splice( index, 1 );
                 }
-
-                function socketConnectionAttempt(ip,port,family)
-                {
-                    console.log("socketConnectionAttempt",ip,port,family)
-                    msg.ping.ip     = ip;
-                    msg.ping.family = family;
-                }
-
-                function socketConnect()
-                {
-                    console.log("socketConnect")
-                    socket.end();
-                    sendResult( Date.now() - start );
-                }
-
-                function socketError(event)
-                {
-                    console.log("socketError",event.message)
-                    msg.ping.error = event.message;
-                    sendResult( false );
-                }
-
-                socket.addListener( "connectionAttempt", socketConnectionAttempt );
-                socket.addListener( "connectionAttemptFailed", socketConnectionAttemptFailed );
-                socket.addListener( "connectionAttemptTimeout", socketConnectionAttemptTimeout );
-                socket.addListener( "connect", socketConnect );
-                socket.addListener( "close", socketClose );
-                socket.addListener( "error", socketError );
-                const start  = Date.now();
-                socket.connect( {
-                    family: node.family,
-                    host:   msg.ping.host,
-                    port:   msg.ping.port
+                node.status( {
+                    fill:  color,
+                    shape: "dot",
+                    text:  msg.ping.host
                 } );
+                done();
+            }
+
+            function socketConnectionAttempt(ip,port,family)
+            {
+                console.log("socketConnectionAttempt",ip,port,family)
+                msg.ping.ip     = ip;
+                msg.ping.family = family;
+            }
+
+            function socketConnect()
+            {
+                console.log("socketConnect")
+                socket.end();
+                sendResult( Date.now() - start, "green" );
+            }
+
+            function socketError(event)
+            {
+                let color;
+                console.log("socketError",event.code)
+                if( event.errno !== -113 )
+                {
+                    node.error( event.message );
+                    color = "gray";
+                }
+                else
+                {
+                    color = "red";
+                }
+                msg.ping.error = event.message;
+                sendResult( false, color );
+            }
+
+            socket.addListener( "connectionAttempt", socketConnectionAttempt );
+            socket.addListener( "connect", socketConnect );
+            socket.addListener( "error", socketError );
+
+            const start = Date.now();
+            node.openSockets.push( socket );
+            socket.connect( {
+                family: family,
+                host:   msg.ping.host,
+                port:   msg.ping.port
+            } );
         });
 
         node.on('close', function() {
@@ -75,21 +128,6 @@ module.exports = function(RED) {
                 i.destroy();
             }
         });
-    }
-
-    function socketConnectionAttemptFailed(event)
-    {
-        console.log("socketConnectionAttemptFailed",event)
-    }
-
-    function socketConnectionAttemptTimeout(event)
-    {
-        console.log("socketConnectionAttemptTimeout",event)
-    }
-
-    function socketClose(hadError)
-    {
-        console.log("socketClose",hadError)
     }
 
     RED.nodes.registerType( "tcPing", TcPingNode );
--- node-red-tcping/test/tcping_spec.js
+++ node-red-tcping/test/tcping_spec.js
@@ -97,6 +97,238 @@ describe( 'tcPing Node', function () {
     });
   });
 
+  it('should ping a fixed URL, IPv4', function (done) {
+    var flow = [{ id: 'flow', type: 'tab' },
+                { id: "n1", type: "tcPing", host: "www.google.com", port: "443", family: "4", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg);
+        try {
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload').which.is.within(1,150);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','www.google.com');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family',4);
+          msg.ping.should.have.property('ip');
+          net.isIPv4( msg.ping.ip ).should.match( true );
+          msg.ping.should.not.have.property('error');
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try{
+        n1.should.have.a.property('name', 'test');
+        n1.should.have.a.property('host','www.google.com');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 4);
+        await delay(50);
+        n1.receive({ topic:"foobar" });
+        await delay(300);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should ping a fixed URL, IPv6', function (done) {
+    var flow = [{ id: 'flow', type: 'tab' },
+                { id: "n1", type: "tcPing", host: "www.google.com", port: "443", family: "6", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg);
+        try {
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload').which.is.within(1,150);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','www.google.com');
+          msg.ping.should.have.property('port',443);
+          msg.ping.should.have.property('family',6);
+          msg.ping.should.have.property('ip');
+          net.isIPv6( msg.ping.ip ).should.match( true );
+          msg.ping.should.not.have.property('error');
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try{
+        n1.should.have.a.property('name', 'test');
+        n1.should.have.a.property('host','www.google.com');
+        n1.should.have.a.property('port', 443);
+        n1.should.have.a.property('family', 6);
+        await delay(50);
+        n1.receive({ topic:"foobar" });
+        await delay(300);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should ping with string payload', function (done) {
+    var flow = [{ id: 'flow', type: 'tab' },
+                { id: "n1", type: "tcPing", family: "0", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg);
+        try {
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload').which.is.within(1,150);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','www.google.de');
+          msg.ping.should.have.property('port',c==1?443:80);
+          msg.ping.should.have.property('family').which.is.within(4,6);
+          msg.ping.should.have.property('ip').which.is.a.String();
+          msg.ping.should.not.have.property('error');
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try{
+        n1.should.have.a.property('name', 'test');
+        n1.should.have.a.property('host','');
+        n1.should.have.a.property('port', 80);
+        n1.should.have.a.property('family', 0);
+        await delay(50);
+        n1.receive({ topic:"foobar", payload:"www.google.de" });
+        await delay(300);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
+        n1.receive({ topic:"foobar", payload:"www.google.de:443" });
+        await delay(300);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 2 );
+        n1.receive({ topic:"foobar", payload:"" });
+        await delay(50);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(1);
+        c.should.match( 2 );
+        n1.receive({ topic:"foobar", payload:"www.google.de:" });
+        n1.receive({ topic:"foobar", payload:"www.google.de:0" });
+        n1.receive({ topic:"foobar", payload:"www.google.de:-1" });
+        n1.receive({ topic:"foobar", payload:"www.google.de:0x10000" });
+        n1.receive({ topic:"foobar", payload:"www.google.de:foobar" });
+        n1.receive({ topic:"foobar", payload:"www.google.de:80.5" });
+        await delay(100);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(7);
+        c.should.match( 2 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should ping with object payload', function (done) {
+    var flow = [{ id: 'flow', type: 'tab' },
+                { id: "n1", type: "tcPing", family: "0", family: "4", name: "test", wires: [["n2"]], z: "flow" },
+                { id: "n2", type: "helper", z: "flow" }];
+    helper.load(node, flow, async function () {
+      var n2 = helper.getNode("n2");
+      var n1 = helper.getNode("n1");
+      var c = 0;
+      n2.on("input", function (msg) {
+        console.log(msg);
+        try {
+          msg.should.have.property('topic',"foobar");
+          msg.should.have.property('payload').which.is.within(1,150);
+          msg.should.have.property('ping').which.is.an.Object();
+          msg.ping.should.have.property('host','www.nodejs.org');
+          msg.ping.should.have.property('port',c==1?443:80);
+          msg.ping.should.have.property('family',c==1?6:4);
+          msg.ping.should.have.property('ip').which.is.a.String();
+          msg.ping.should.not.have.property('error');
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try{
+        n1.should.have.a.property('name', 'test');
+        n1.should.have.a.property('host','');
+        n1.should.have.a.property('port', 80);
+        n1.should.have.a.property('family', 4);
+        await delay(50);
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org"} });
+        await delay(300);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",port:443,family:6} });
+        await delay(300);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 2 );
+        n1.receive({ topic:"foobar", payload:{} });
+        await delay(50);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(1);
+        c.should.match( 2 );
+        n1.receive({ topic:"foobar", payload:{host:""} });
+        await delay(50);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(2);
+        c.should.match( 2 );
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",port:0} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",port:-1} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",port:0x10000} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",port:"foobar"} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",port:80.5} });
+        await delay(100);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(7);
+        c.should.match( 2 );
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",family:"foobar"} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",family:-1} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",family:1} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",family:3} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",family:5} });
+        n1.receive({ topic:"foobar", payload:{host:"www.nodejs.org",family:7} });
+        await delay(100);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(13);
+        c.should.match( 2 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
   it('should ping an invalid URL', function (done) {
     var flow = [{ id: 'flow', type: 'tab' },
                 { id: "n1", type: "tcPing", host: "foo:bar", port:"443", name: "test", wires: [["n2"]], z: "flow" },
@@ -131,7 +363,7 @@ describe( 'tcPing Node', function () {
         n1.receive({ topic:"foobar" });
         await delay(50);
         n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
+        n1.error.should.have.callCount(1);
         c.should.match( 1 );
         done();
       }
@@ -175,7 +407,7 @@ describe( 'tcPing Node', function () {
         n1.receive({ topic:"foobar" });
         await delay(100);
         n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
+        n1.error.should.have.callCount(1);
         c.should.match( 1 );
         done();
       }
@@ -219,7 +451,7 @@ describe( 'tcPing Node', function () {
         n1.receive({ topic:"foobar" });
         await delay(50);
         n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
+        n1.error.should.have.callCount(1);
         c.should.match( 1 );
         done();
       }

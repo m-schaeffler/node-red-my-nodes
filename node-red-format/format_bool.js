fa01d601678206ce07ed75f434a921f9bb5e5565
Mathias Schäffler <mathias@fam-schaeffler.de>
2026-05-12 08:55:55 +0200

    formatBool: scaffolding

---
+++ node-red-format/format_bool.js
@@ -0,0 +1,98 @@
+module.exports = function(RED) {
+
+    function FormatBoolNode(config) {
+        RED.nodes.createNode(this,config);
+        //this.config = config;
+        var node = this;
+        this.property     = config.property ?? "payload";
+        this.propertyType = config.propertyType ?? "msg";
+        this.falseValue   = config.falseValue ?? "0";
+        this.trueValue    = config.trueValue ?? "1";
+        this.timeout      = Number( config.timneout ?? 0 );
+        this.timeoutValue = config.timeoutValue ?? "";
+        this.showState    = Boolean( config.showState );
+        this.filter       = Boolean( config.filter );
+        this.last         = undefined;
+        if( this.propertyType === "jsonata" )
+        {
+            try {
+                this.propertyPrepared = RED.util.prepareJSONataExpression( this.property, this );
+            }
+            catch (e) {
+                node.error(RED._("debug.invalid-exp", {error: this.property}));
+                return;
+            }
+        }
+        node.status( "" );
+
+        node.on('input', function(msg,send,done) {
+            if( msg.invalid )
+            {
+                done();
+                return;
+            }
+            function getPayload(callback)
+            {
+                if( node.propertyPrepared )
+                {
+                    RED.util.evaluateJSONataExpression( node.propertyPrepared, msg, function(err, value)
+                    {
+                        if( err )
+                        {
+                            //done( err.message );
+                            node.error( err.message );
+                            callback( null );
+                        }
+                        else
+                        {
+                            callback( value === undefined ? null : value );
+                        }
+                    } );
+                }
+                else
+                {
+                    try
+                    {
+                        callback( RED.util.getMessageProperty( msg, node.property ) );
+                    }
+                    catch( err )
+                    {
+                        node.error( err.message );
+                        callback( null );
+                    }
+                }
+            }
+            getPayload( function(value)
+            {
+                let status = {};
+                msg.payload = Boolean( value );
+                if( node.filter )
+                {
+                    status.shape = "dot";
+                    if( msg.payload !== node.last )
+                    {
+                        node.last = msg.payload;
+                        status.fill = "green";
+                        send( msg );
+                    }
+                    else
+                    {
+                        status.fill = "gray";
+                    }
+                }
+                else
+                {
+                    send( msg );
+                }
+                if( node.showState )
+                {
+                    status.text = msg.payload;
+                    node.status( status );
+                }
+                done();
+            } );
+        });
+    }
+
+    RED.nodes.registerType("formatBool",FormatBoolNode);
+}

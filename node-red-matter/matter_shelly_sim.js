e8ec0381561e933dc670b1e02259a19073ee5407
Mathias Schäffler <mathias@fam-schaeffler.de>
2026-07-17 09:06:35 +0200

    matterShellySim: UTs for simple data

--- node-red-matter/matter_shelly_sim.js
+++ node-red-matter/matter_shelly_sim.js
@@ -6,6 +6,101 @@ module.exports = function(RED) {
         node.status( "" );
 
         node.on('input', function(msg,send,done) {
+            function sendCommand(command,data=null)
+            {
+                send( {
+                    topic:   msg.topic,
+                    payload: {
+                        command: command,
+                        data:    data
+                    }
+                } );
+            }
+
+            function setBoolean(value)
+            {
+                switch( value )
+                {
+                    case 1:
+                    case "1":
+                    case "on":
+                    case true:
+                    case "true":
+                        sendCommand( "onoff.on" );
+                        break;
+                    case 0:
+                    case "0":
+                    case "off":
+                    case false:
+                    case "false":
+                    case "disabled":
+                        sendCommand( "onoff.off" );
+                        break;
+                    case "toggle":
+                        sendCommand( "onoff.toggle" );
+                        break;
+                    default:
+                        node.error( "invalid value "+value );
+                }
+            }
+
+            switch( msg.payload.command )
+            {
+                case "relay":
+                case "switch":
+                case "output":
+                case "light":
+                    switch( typeof msg.payload.data )
+                    {
+                        case "boolean":
+                        case "number":
+                        case "string":
+                            setBoolean( msg.payload.data );
+                            break;
+                        case "object":
+                            for( const i in msg.payload.data )
+                            {
+                                switch( i )
+                                {
+                                    case "turn":
+                                        setBoolean( msg.payload.data.turn );
+                                        break;
+                                    case "on":
+                                        setBoolean( msg.payload.data.on );
+                                        break;
+                                    case "brightness":
+                                        sendCommand( "levelcontrol.movetolevel", { level: Math.round(msg.payload.data.brightness*2.54), transitionTime: msg.payload.data.transition ?? null, optionsMask: 0x00, optionsOverride: 0x00 } );
+                                        break;
+                                    case "transition":
+                                        break;
+                                    default:
+                                        node.error( "invalid attribute " + i );
+                                }
+                            }
+                            break;
+                    }
+                    break;
+                case "roller":
+                case "cover":
+                    switch( msg.payload.data )
+                    {
+                        case "open":
+                            sendCommand( "windowcovering.open" );
+                            break;
+                        case "close":
+                            sendCommand( "windowcovering.close" );
+                            break;
+                        case "stop":
+                            sendCommand( "windowcovering.stop" );
+                            break;
+                    }
+                    break;
+                case "position":
+                    sendCommand( "windowcovering.gotolift", msg.payload.data );
+                    break;
+                default:
+                    send( msg );
+            }
             done();
         });
     }
--- node-red-matter/test/matter_shelly_sim_spec.js
+++ node-red-matter/test/matter_shelly_sim_spec.js
@@ -39,4 +39,211 @@ describe( 'matter_shelly_sim Node', function () {
     });
   });
 
+  it('should just forward unknown messages', function (done) {
+    var flow = [{ id: "n1", type: "matterShellySim", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, async function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var c  = 0;
+      n2.on("input", function (msg) {
+        try {
+          //console.log(msg)
+          msg.should.have.a.property('topic',"FooBar");
+          msg.should.have.a.property('payload',{ command:"levelcontrol.movetolevel", data: 50 });
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('name', 'test');
+        await delay(50);
+        n1.receive({ topic:"FooBar", payload: { command:"levelcontrol.movetolevel", data: 50 } });
+        await delay(50);
+        n1.log.should.have.callCount(0);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 1 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should convert output messages with simple data', function (done) {
+    var flow = [{ id: "n1", type: "matterShellySim", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, async function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var c  = 0;
+      n2.on("input", function (msg) {
+        try {
+          //console.log(msg)
+          msg.should.have.a.property('topic',"Output");
+          msg.should.have.a.property('payload',{ command:c<12?(c%2?"onoff.off":"onoff.on"):"onoff.toggle", data: null });
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('name', 'test');
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: true } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: false } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "true" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "false" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: 1 } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: 0 } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "1" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "0" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "on" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "off" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "on" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "disabled" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "toggle" } });
+        await delay(50);
+        n1.receive({ topic:"Output", payload: { command:"output", data: "invalid" } });
+        await delay(50);
+        n1.log.should.have.callCount(0);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(1);
+        c.should.match( 13 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should convert relay messages', function (done) {
+    var flow = [{ id: "n1", type: "matterShellySim", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, async function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var c  = 0;
+      n2.on("input", function (msg) {
+        try {
+          //console.log(msg)
+          msg.should.have.a.property('topic',"Relay");
+          msg.should.have.a.property('payload',{ command: c%2?"onoff.off":"onoff.on", data: null });
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('name', 'test');
+        await delay(50);
+        n1.receive({ topic:"Relay", payload: { command:"relay", data: true } });
+        await delay(50);
+        n1.receive({ topic:"Relay", payload: { command:"relay", data: false } });
+        await delay(50);
+        n1.log.should.have.callCount(0);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 2 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should convert switch messages', function (done) {
+    var flow = [{ id: "n1", type: "matterShellySim", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, async function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var c  = 0;
+      n2.on("input", function (msg) {
+        try {
+          //console.log(msg)
+          msg.should.have.a.property('topic',"Relay");
+          msg.should.have.a.property('payload',{ command: c%2?"onoff.off":"onoff.on", data: null });
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('name', 'test');
+        await delay(50);
+        n1.receive({ topic:"Relay", payload: { command:"relay", data: true } });
+        await delay(50);
+        n1.receive({ topic:"Relay", payload: { command:"relay", data: false } });
+        await delay(50);
+        n1.log.should.have.callCount(0);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 2 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
+  it('should convert light messages with simple data', function (done) {
+    var flow = [{ id: "n1", type: "matterShellySim", name: "test", wires: [["n2"]] },
+                { id: "n2", type: "helper" }];
+    helper.load(node, flow, async function () {
+      var n1 = helper.getNode("n1");
+      var n2 = helper.getNode("n2");
+      var c  = 0;
+      n2.on("input", function (msg) {
+        try {
+          //console.log(msg)
+          msg.should.have.a.property('topic',"Relay");
+          msg.should.have.a.property('payload',{ command: c%2?"onoff.off":"onoff.on", data: null });
+          ++c;
+        }
+        catch(err) {
+          done(err);
+        }
+      });
+      try {
+        n1.should.have.a.property('name', 'test');
+        await delay(50);
+        n1.receive({ topic:"Relay", payload: { command:"relay", data: true } });
+        await delay(50);
+        n1.receive({ topic:"Relay", payload: { command:"relay", data: false } });
+        await delay(50);
+        n1.log.should.have.callCount(0);
+        n1.warn.should.have.callCount(0);
+        n1.error.should.have.callCount(0);
+        c.should.match( 2 );
+        done();
+      }
+      catch(err) {
+        done(err);
+      }
+    });
+  });
+
 });

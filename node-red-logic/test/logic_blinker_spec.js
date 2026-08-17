35eec2fe040db34265a00f5dd49e24787b195f99
Mathias Schäffler <mathias@fam-schaeffler.de>
2026-08-17 12:32:42 +0200

    blinker: more UTs

--- node-red-logic/test/logic_blinker_spec.js
+++ node-red-logic/test/logic_blinker_spec.js
@@ -201,202 +201,52 @@ describe( 'logic_blinker Node', function () {
       }
     });
   });
-/*
-  it('should delay switching on, output number', function (done) {
-    var flow = [{ id: "n1", type: "timerelay", delay:250, delayUnit:"msec", postrun:0, outputOn:"1", outputOnType:"num", outputOff:"0", outputOffType:"num", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        c++;
-        try {
-          msg.should.have.a.property('payload',Number( c<3 ? Boolean(c%2) : false ));
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('delay', 250);
-        n1.should.have.a.property('postrun', 0);
-        n1.should.have.a.property('outputOn', 1);
-        n1.should.have.a.property('outputOff', 0);
-        await delay(50);
-        // normal
-        n1.receive({ payload: 1 });
-        await delay(225);
-        c.should.match( 0 );
-        await delay(50);
-        c.should.match( 1 );
-        n1.receive({ payload: 0 });
-        await delay(50);
-        c.should.match( 2 );
-        // change of mind
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 2 );
-        n1.receive({ payload: 0 });
-        await delay(50);
-        c.should.match( 3 );
-        await delay(250);
-        c.should.match( 3 );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
 
-  it('should delay switching off, output string', function (done) {
-    var flow = [{ id: "n1", type: "timerelay", delay:0, postrun:250, postrunUnit:"msec", outputOn:"on", outputOnType:"str", outputOff:"off", outputOffType:"str", name: "test", wires: [["n2"]] },
+  it('should blink, off in pause phase', function (done) {
+    var flow = [{ id: "n1", type: "blinker", onTime:150, onTimeUnit:"msecs", pauseTime:100, pauseTimeUnit:"msecs", outputOn:'on', outputOnType:"str", outputPause:'pause', outputPauseType:"str",  outputOff:'off', outputOffType:"str", name: "test", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
     helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
       var c = 0;
       n2.on("input", function (msg) {
+        console.log(msg)
         c++;
         try {
-          msg.should.have.a.property('payload',( c<4 ? Boolean(c%2) : true ) ? "on" : "off" );
+          msg.should.have.a.property('payload',c==9?"off":c%2?"on":"pause");
         }
         catch(err) {
           done(err);
         }
       });
       try {
-        n1.should.have.a.property('delay', 0);
-        n1.should.have.a.property('postrun', 250);
+        n1.should.have.a.property('property', 'payload');
+        //n1.should.have.a.property('propertyType', 'msg');
+        n1.should.have.a.property('onTime', 150);
+        n1.should.have.a.property('pauseTime', 100);
         n1.should.have.a.property('outputOn', "on");
+        n1.should.have.a.property('outputPause', "pause");
         n1.should.have.a.property('outputOff', "off");
+        n1.should.have.a.property('showState', false);
         await delay(50);
-        // normal
         n1.receive({ payload: 1 });
-        await delay(50);
+        await delay(25);
         c.should.match( 1 );
-        n1.receive({ payload: 0 });
-        await delay(225);
-        c.should.match( 1 );
-        await delay(50);
-        c.should.match( 2 );
-        // change of mind
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 3 );
-        n1.receive({ payload: 0 });
-        await delay(50);
-        c.should.match( 3 );
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 4 );
-        await delay(250);
-        c.should.match( 4 );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-
-  it('should delay switching on and off, output json', function (done) {
-    const jsonOn  = { value:true, num:42 };
-    const jsonOff = { value:false, num:-1 };
-    var flow = [{ id: "n1", type: "timerelay", delay:250, delayUnit:"msec", postrun:250, postrunUnit:"msec", outputOn:JSON.stringify(jsonOn), outputOnType:"json", outputOff:JSON.stringify(jsonOff), outputOffType:"json", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        c++;
-        try {
-          msg.should.have.a.property('payload',c%2 ? jsonOn : jsonOff );
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('delay', 250);
-        n1.should.have.a.property('postrun', 250);
-        n1.should.have.a.property('outputOn', jsonOn);
-        n1.should.have.a.property('outputOff', jsonOff);
-        await delay(50);
-        n1.receive({ payload: 1 });
-        await delay(225);
-        c.should.match( 0 );
-        await delay(50);
-        c.should.match( 1 );
-        n1.receive({ payload: 0 });
-        await delay(225);
+        await delay(100);
         c.should.match( 1 );
         await delay(50);
         c.should.match( 2 );
-        n1.warn.should.have.callCount(0);
-        n1.error.should.have.callCount(0);
-        done();
-      }
-      catch(err) {
-        done(err);
-      }
-    });
-  });
-
-  it('should not have max on time', function (done) {
-    var flow = [{ id: "n1", type: "timerelay", maxOn:150, maxOnUnit:"msec", name: "test", wires: [["n2"]] },
-                { id: "n2", type: "helper" }];
-    helper.load(node, flow, async function () {
-      var n2 = helper.getNode("n2");
-      var n1 = helper.getNode("n1");
-      var c = 0;
-      n2.on("input", function (msg) {
-        c++;
-        try {
-          msg.should.have.a.property('payload',Boolean(c%2));
-        }
-        catch(err) {
-          done(err);
-        }
-      });
-      try {
-        n1.should.have.a.property('maxOn', 150);
-        await delay(50);
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 1 );
-        n1.receive({ payload: 0 });
         await delay(50);
         c.should.match( 2 );
-        n1.receive({ payload: 1 });
         await delay(50);
         c.should.match( 3 );
-        await delay(75);
-        c.should.match( 3 );
-        await delay(50);
-        c.should.match( 4 );
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 5 );
-        n1.receive({ payload: 0 });
-        await delay(50);
-        c.should.match( 6 );
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 7 );
-        await delay(75);
-        c.should.match( 7 );
-        await delay(50);
+        await delay(650);
         c.should.match( 8 );
         n1.receive({ payload: 0 });
         await delay(50);
-        c.should.match( 8 );
+        c.should.match( 9 );
+        await delay(500);
+        c.should.match( 9 );
         n1.warn.should.have.callCount(0);
         n1.error.should.have.callCount(0);
         done();
@@ -407,49 +257,46 @@ describe( 'logic_blinker Node', function () {
     });
   });
 
-  it('should not have min on time', function (done) {
-    var flow = [{ id: "n1", type: "timerelay", minOn:150, minOnUnit:"msec", name: "test", wires: [["n2"]] },
+  it('should blink forever', function (done) {
+    var flow = [{ id: "n1", type: "blinker", onTime:150, onTimeUnit:"msecs", pauseTime:100, pauseTimeUnit:"msecs", outputOn:'on', outputOnType:"str", outputPause:'pause', outputPauseType:"str",  outputOff:'off', outputOffType:"str", name: "test", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
     helper.load(node, flow, async function () {
       var n2 = helper.getNode("n2");
       var n1 = helper.getNode("n1");
       var c = 0;
       n2.on("input", function (msg) {
+        console.log(msg)
         c++;
         try {
-          msg.should.have.a.property('payload',Boolean(c%2));
+          msg.should.have.a.property('payload',c==10?"off":c%2?"on":"pause");
         }
         catch(err) {
           done(err);
         }
       });
       try {
-        n1.should.have.a.property('minOn', 150);
+        n1.should.have.a.property('property', 'payload');
+        //n1.should.have.a.property('propertyType', 'msg');
+        n1.should.have.a.property('onTime', 150);
+        n1.should.have.a.property('pauseTime', 100);
+        n1.should.have.a.property('outputOn', "on");
+        n1.should.have.a.property('outputPause', "pause");
+        n1.should.have.a.property('outputOff', "off");
+        n1.should.have.a.property('showState', false);
         await delay(50);
         n1.receive({ payload: 1 });
-        await delay(50);
+        await delay(25);
         c.should.match( 1 );
-        await delay(200);
+        await delay(100);
         c.should.match( 1 );
-        n1.receive({ payload: 0 });
         await delay(50);
         c.should.match( 2 );
-        n1.receive({ payload: 1 });
-        n1.receive({ payload: 0 });
+        await delay(50);
+        c.should.match( 2 );
         await delay(50);
         c.should.match( 3 );
-        await delay(75);
-        c.should.match( 3 );
-        await delay(50);
-        c.should.match( 4 );
-        n1.receive({ payload: 1 });
-        await delay(50);
-        c.should.match( 5 );
-        await delay(200);
-        c.should.match( 5 );
-        n1.receive({ payload: 0 });
-        await delay(50);
-        c.should.match( 6 );
+        await delay(800);
+        c.should.match( 9 );
         n1.warn.should.have.callCount(0);
         n1.error.should.have.callCount(0);
         done();
@@ -459,7 +306,7 @@ describe( 'logic_blinker Node', function () {
       }
     });
   });
-*/
+
   it('should work with objects', function (done) {
     var flow = [{ id: "n1", type: "blinker", name: "test", property:"payload.value", wires: [["n2"]] },
                 { id: "n2", type: "helper" }];
